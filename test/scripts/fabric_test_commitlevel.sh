fe6075ced49eaf3897254f925cb7bf832bfbcd9d
Divyank Katira <Divyank.Katira@securekey.com>
2017-06-15 09:26:48 -0400

    [FAB-4645] Remove commit level script
    Replaced latest-env.sh, fixed vendor conflict
    
    Change-Id: I792f64aa8741773c46f0c8c9800f5d53806a97b1
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

--- test/scripts/fabric_test_commitlevel.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#file used for automatic integration build test
-#This should always match what's in the README.md
-export FABRIC_COMMIT=v1.0.0-alpha2
-export FABRIC_CA_COMMIT=v1.0.0-alpha2
