0f081b38a3e13421e723a9bc949cbc0e4168c5c0
Troy Ronda <troy@troyronda.com>
2017-07-07 17:26:14 -0400

    [FAB-5215] WIP: SDK entry point
    Change-Id: Ia839c3602be257d2ed979f0573a0b38d0e8cbc06
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ api/apitxn/apitxn.go
@@ -0,0 +1,12 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+// Package apitxn allows SDK users to plugin their own implementations of transaction execution.
+package apitxn
+
+// Executor encapsulates query, transaction and filter execution
+type Executor interface {
+}
