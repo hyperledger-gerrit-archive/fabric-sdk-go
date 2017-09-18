b9a25fa95b89dd24afc83b77ce89f6dcdbe3dda2
Troy Ronda <troy.ronda@securekey.com>
2017-09-18 15:35:59 -0400

    [FAB-6184] Improve Fabric vendoring (populate)
    Change-Id: Idd8d0cd17033fa60a3bd39bc6e2d4942d97f87aa
    Signed-off-by: Troy Ronda <troy.ronda@securekey.com>

---
+++ internal/github.com/hyperledger/fabric/common/channelconfig/stub.go
@@ -0,0 +1,13 @@
+/*
+Copyright IBM Corp. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package channelconfig
+
+import (
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/common/flogging"
+)
+
+var logger = flogging.MustGetLogger("common/channelconfig")
