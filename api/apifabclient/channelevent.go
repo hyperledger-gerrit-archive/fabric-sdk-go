be71747342bcab461d776eba3b419125703326ff
Bob Stasyszyn <bob.stasyszyn@securekey.com>
2017-09-21 09:19:59 -0400

    WIP - [FAB-6243] - Channel Event Client
    Implement a Channel Event Client that connects to
    the Channel Service endpoint in Fabric.
    
    Note: Some files have been tagged as 'channelevents'
    since the channel service has not yet been merged
    into Fabric.
    
    To test, cherry pick the following in fabric:
    
    - 12375, 12377, 12379, 12381, 12483, 13663, 12609, 13001
    
    ... and run the test (from test/integration):
    
    $ go test -run TestChannelEvents -tags channelevents
    
    Change-Id: I92e349f3cbdf39308f45b683aeadce788749640d
    Signed-off-by: Bob Stasyszyn <bob.stasyszyn@securekey.com>

---
+++ api/apifabclient/channelevent.go
@@ -0,0 +1,87 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package apifabclient
+
+import (
+	"github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/common"
+	pb "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/peer"
+)
+
+// BlockEvent contains the data for the block event
+type BlockEvent struct {
+	Block *common.Block
+}
+
+// TxStatusEvent contains the data for a transaction status event
+type TxStatusEvent struct {
+	TxID             string
+	TxValidationCode pb.TxValidationCode
+}
+
+// CCEvent contains the data for a chaincocde event
+type CCEvent struct {
+	TxID        string
+	ChaincodeID string
+	EventName   string
+}
+
+// ConnectionEvent is sent when the client disconnects or reconnects to the peer
+type ConnectionEvent struct {
+	Connected bool
+	Err       error
+}
+
+// Registration is a handle that is returned from a successful RegisterXXXEvent.
+// This handle should be used in Unregister in order to unregister the event.
+type Registration interface{}
+
+// RegistrationResponse is the response that is returned for any register/unregister event.
+// For a successful registration, the registration handle is set. This handle should be used
+// in a subsequent Unregister request. If an error occurs then the error is set.
+type RegistrationResponse struct {
+	// Reg is a handle to the registration
+	Reg Registration
+
+	// Err contains the error if registration is unsuccessful
+	Err error
+}
+
+// ChannelEventClient is a client that is used to connect to a peer and receive channel events,
+// such as block, chaincode, and transaction status events.
+type ChannelEventClient interface {
+	// Connect connects to the peer and registers for channel events on a particular channel.
+	Connect() error
+
+	// Disconnect disconnects from the peer. Once this function is invoke the client may no longer be used.
+	Disconnect()
+
+	// RegisterBlockEvent registers for block events. If the client is not authorized to receive
+	// block events then an error is returned.
+	// - eventch is the Go channel to which events are sent. Note that the events should be processed
+	//         in a separate Go routine so that the event dispatcher is not blocked.
+	// Example:
+	//
+	RegisterBlockEvent(eventch chan<- *BlockEvent) (Registration, error)
+
+	// RegisterChaincodeEvent registers for chaincode events. If the client is not authorized to receive
+	// chaincode events then an error is returned.
+	// - ccID is the chaincode ID for which events are to be received
+	// - eventFilter is the chaincode event filter (regular expression) for which events are to be received
+	// - eventch is the Go channel to which events are sent. Note that the events should be processed
+	//         in a separate Go routine so that the event dispatcher is not blocked.
+	RegisterChaincodeEvent(ccID, eventFilter string, eventch chan<- *CCEvent) (Registration, error)
+
+	// RegisterTxStatusEvent registers for transaction status events. If the client is not authorized to receive
+	// transaction status events then an error is returned.
+	// - txID is the transaction ID for which events are to be received
+	// - eventch is the Go channel to which events are sent. Note that the events should be processed in a separate Go routine so that the event dispatcher is not blocked.
+	RegisterTxStatusEvent(txID string, eventch chan<- *TxStatusEvent) (Registration, error)
+
+	// Unregister unregisters the given registration.
+	// - reg is the registration handle that was returned from one of the RegisterXXX functions
+	Unregister(reg Registration) error
+}
---
+++ pkg/fabric-client/channelevents/mockconnection.go
@@ -0,0 +1,314 @@
+// +build channelevents
+
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package channelevents
+
+import (
+	"reflect"
+	"sync"
+
+	"github.com/hyperledger/fabric-sdk-go/api/apiconfig"
+	"github.com/hyperledger/fabric-sdk-go/api/apifabclient"
+	"github.com/hyperledger/fabric-sdk-go/pkg/errors"
+	pb "github.com/hyperledger/fabric-sdk-go/third_party/github.com/hyperledger/fabric/protos/peer"
+)
+
+// Operation is the operation being performed
+type Operation string
+
+// Result is the result to take for a given operation
+type Result string
+
+const (
+	// RegisterChannel is the Register Channel operation (used in the OperationMap)
+	RegisterChannel Operation = "reg-channel"
+
+	// UnregisterChannel is the Unregister Channel operation (used in the OperationMap)
+	UnregisterChannel Operation = "unreg-channel"
+
+	// SucceedResult indicates that the operation should succeed
+	SucceedResult Result = "succeed"
+
+	// FailResult indicates that the operation should fail
+	FailResult Result = "fail"
+
+	// NoOpResult indicates that the operation should be ignored (i.e. just do nothing)
+	// This should result in the client timing out waiting for a response.
+	NoOpResult Result = "no-op"
+
+	// InvalidChannelResult indicates that the operation should use an invalid channel ID
+	InvalidChannelResult Result = "invalid-channel"
+)
+
+// MockConnection is a fake connection used for unit testing
+type MockConnection struct {
+	Operations       OperationMap
+	AuthorizedEvents []string
+	rcvch            chan interface{}
+}
+
+// NewMockConnection returns a new MockConnection using the given options
+func NewMockConnection(opts ...MockConnOpt) *MockConnection {
+	conn := &MockConnection{
+		rcvch:      make(chan interface{}),
+		Operations: make(map[Operation]ResultDesc),
+	}
+	for _, opt := range opts {
+		opt.Apply(conn)
+	}
+	return conn
+}
+
+// Close implements the Connection interface
+func (c *MockConnection) Close() {
+	if c.rcvch != nil {
+		close(c.rcvch)
+	}
+}
+
+// Send implements the Connection interface
+func (c *MockConnection) Send(emsg *pb.Event) error {
+	if c.rcvch == nil {
+		return errors.New("mock connection not initialized")
+	}
+
+	switch e := emsg.Event.(type) {
+	case *pb.Event_RegisterChannel:
+		result, ok := c.Operations[RegisterChannel]
+		if !ok || result.Result != NoOpResult {
+			channelID := e.RegisterChannel.ChannelIds[0]
+			if result.Result == InvalidChannelResult {
+				channelID = "invalid"
+			}
+			c.ProduceEvent(c.newRegisterChannelResponse(channelID))
+		}
+	case *pb.Event_DeregisterChannel:
+		result, ok := c.Operations[UnregisterChannel]
+		if !ok || result.Result != NoOpResult {
+			channelID := e.DeregisterChannel.ChannelIds[0]
+			if result.Result == InvalidChannelResult {
+				channelID = "invalid"
+			}
+			c.ProduceEvent(c.newDeregisterChannelResponse(channelID))
+		}
+	default:
+		return errors.Errorf("unsupported event type [%s]", reflect.TypeOf(e))
+	}
+	return nil
+}
+
+// Disconnect implements the Connection interface
+func (c *MockConnection) Disconnect(err error) {
+	c.ProduceEvent(&disconnectedEvent{err: err})
+}
+
+// Receive implements the Connection interface
+func (c *MockConnection) Receive(eventch chan<- interface{}) {
+	for {
+		e, ok := <-c.rcvch
+		if !ok {
+			break
+		}
+
+		eventch <- e
+	}
+}
+
+// ProduceEvent allows a unit test to inject an event
+func (c *MockConnection) ProduceEvent(event interface{}) {
+	go func() {
+		c.rcvch <- event
+	}()
+}
+
+func (c *MockConnection) newRegisterChannelResponse(channelID string) *pb.Event_ChannelServiceResponse {
+	success := true
+	errMsg := ""
+	result, ok := c.Operations[RegisterChannel]
+	if ok {
+		success = result.Result == SucceedResult
+		errMsg = result.ErrMsg
+	}
+
+	return &pb.Event_ChannelServiceResponse{
+		ChannelServiceResponse: &pb.ChannelServiceResponse{
+			Success: success,
+			Action:  "RegisterChannel",
+			ChannelServiceResults: []*pb.ChannelServiceResult{
+				&pb.ChannelServiceResult{
+					ChannelId:        channelID,
+					ErrorMsg:         errMsg,
+					AuthorizedEvents: c.AuthorizedEvents,
+				},
+			},
+		},
+	}
+}
+
+func (c *MockConnection) newDeregisterChannelResponse(channelID string) *pb.Event_ChannelServiceResponse {
+	success := true
+	errMsg := ""
+	result, ok := c.Operations[UnregisterChannel]
+	if ok {
+		success = result.Result == SucceedResult
+		errMsg = result.ErrMsg
+	}
+
+	return &pb.Event_ChannelServiceResponse{
+		ChannelServiceResponse: &pb.ChannelServiceResponse{
+			Success: success,
+			Action:  "DeregisterChannel",
+			ChannelServiceResults: []*pb.ChannelServiceResult{
+				&pb.ChannelServiceResult{
+					ChannelId: channelID,
+					ErrorMsg:  errMsg,
+				},
+			},
+		},
+	}
+}
+
+// MockConnectionProviderFactory creates various mock Connection Providers
+type MockConnectionProviderFactory struct {
+	connection *MockConnection
+	mtx        sync.RWMutex
+}
+
+func NewMockConnectionProviderFactory() *MockConnectionProviderFactory {
+	return &MockConnectionProviderFactory{}
+}
+
+// NewMockConnectionProviderFactory returns a new MockConnectionProviderFactory
+func (cp *MockConnectionProviderFactory) Connection() *MockConnection {
+	cp.mtx.RLock()
+	defer cp.mtx.RUnlock()
+	return cp.connection
+}
+
+// Provider returns a connection provider that always returns the given connection
+func (cp *MockConnectionProviderFactory) Provider(conn *MockConnection) ConnectionProvider {
+	return func(apifabclient.FabricClient, *apiconfig.PeerConfig) (Connection, error) {
+		return conn, nil
+	}
+}
+
+// FlakeyProvider creates a connection provider that returns a connection according to the given
+// connection attempt results. The results tell the connection provider whether or not to fail,
+// to return a connection, what authorization to give the connection, etc.
+func (cp *MockConnectionProviderFactory) FlakeyProvider(connAttemptResults ConnectAttemptResults) ConnectionProvider {
+	var connectAttempt ConnectionAttempt
+	return func(apifabclient.FabricClient, *apiconfig.PeerConfig) (Connection, error) {
+		connectAttempt++
+
+		result, ok := connAttemptResults[connectAttempt]
+		if !ok {
+			return nil, errors.New("simulating failed connection attempt")
+		}
+
+		cp.mtx.Lock()
+		defer cp.mtx.Unlock()
+
+		cp.connection = NewMockConnection(NewAuthorizedEventsOpt(result.AuthorizedEvents...))
+		return cp.connection, nil
+	}
+}
+
+// ConnectionAttempt specifies the number of connection attempts
+type ConnectionAttempt uint
+
+// ConnectResult contains the data to use for the N'th connection attempt
+type ConnectResult struct {
+	Attempt          ConnectionAttempt
+	AuthorizedEvents []string
+}
+
+// NewConnectResult returns a new ConnectResult
+func NewConnectResult(attempt ConnectionAttempt, authorizedEvents ...string) ConnectResult {
+	return ConnectResult{Attempt: attempt, AuthorizedEvents: authorizedEvents}
+}
+
+// ConnectAttemptResults maps a connection attempt to a connection result
+type ConnectAttemptResults map[ConnectionAttempt]ConnectResult
+
+// NewConnectResults returns a new ConnectAttemptResults
+func NewConnectResults(results ...ConnectResult) ConnectAttemptResults {
+	mapResults := make(map[ConnectionAttempt]ConnectResult)
+	for _, r := range results {
+		mapResults[r.Attempt] = r
+	}
+	return mapResults
+}
+
+// ResultDesc describes the result of an operation and optional error string
+type ResultDesc struct {
+	Result Result
+	ErrMsg string
+}
+
+// OperationMap maps an Operation to a ResultDesc
+type OperationMap map[Operation]ResultDesc
+
+// MockConnOpt applies an option to a MockConnection
+type MockConnOpt interface {
+	// Apply applies the option to the MockConnection
+	Apply(conn *MockConnection)
+}
+
+// AuthorizedEventsOpt is a connection option that applies authorized events to the MockConnection
+type AuthorizedEventsOpt struct {
+	AuthorizedEvents []string
+}
+
+// Apply applies the option to the MockConnection
+func (o *AuthorizedEventsOpt) Apply(conn *MockConnection) {
+	conn.AuthorizedEvents = o.AuthorizedEvents
+}
+
+// NewAuthorizedEventsOpt returns a new AuthorizedEventsOpt
+func NewAuthorizedEventsOpt(authorizedEvents ...string) *AuthorizedEventsOpt {
+	return &AuthorizedEventsOpt{AuthorizedEvents: authorizedEvents}
+}
+
+// OperationResult contains the result of an operation
+type OperationResult struct {
+	Operation  Operation
+	Result     Result
+	ErrMessage string
+}
+
+// NewResult returns a new OperationResult
+func NewResult(operation Operation, result Result, errMsg ...string) *OperationResult {
+	msg := ""
+	if len(errMsg) > 0 {
+		msg = errMsg[0]
+	}
+	return &OperationResult{
+		Operation:  operation,
+		Result:     result,
+		ErrMessage: msg,
+	}
+}
+
+// OperationResultsOpt is a connection option that indicates what to do for each operation
+type OperationResultsOpt struct {
+	Operations OperationMap
+}
+
+// Apply applies the option to the MockConnection
+func (o *OperationResultsOpt) Apply(conn *MockConnection) {
+	conn.Operations = o.Operations
+}
+
+// NewResultsOpt returns a new OperationResultsOpt
+func NewResultsOpt(funcResults ...*OperationResult) *OperationResultsOpt {
+	opt := &OperationResultsOpt{Operations: make(map[Operation]ResultDesc)}
+	for _, fr := range funcResults {
+		opt.Operations[fr.Operation] = ResultDesc{Result: fr.Result, ErrMsg: fr.ErrMessage}
+	}
+	return opt
+}
