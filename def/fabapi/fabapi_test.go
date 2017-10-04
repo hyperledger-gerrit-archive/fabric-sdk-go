95514d2524d9edaf600b5ba46b0d3882cf977fab
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2017-10-04 14:44:40 -0400

    [FAB-6423] Go SDK config reused issue
    Change-Id: I89bc8a91532e8b1ebd4bb79bcf82c27bc405adeb
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- def/fabapi/fabapi_test.go
+++ def/fabapi/fabapi_test.go
@@ -15,11 +15,18 @@ import (
 func TestNewDefaultSDK(t *testing.T) {
 
 	setup := Options{
-		ConfigFile: "../../test/fixtures/config/config_test.yaml",
+		ConfigFile: "../../test/fixtures/config/invalid.yaml",
 		StateStoreOpts: opt.StateStoreOpts{
 			Path: "/tmp/state",
 		},
 	}
+
+	_, err := NewSDK(setup)
+	if err == nil {
+		t.Fatalf("Should have failed for invalid config file")
+	}
+
+	setup.ConfigFile = "../../test/fixtures/config/config_test.yaml"
 
 	sdk, err := NewSDK(setup)
 	if err != nil {
@@ -31,4 +38,42 @@ func TestNewDefaultSDK(t *testing.T) {
 		t.Fatalf("Failed to create new channel client: %s", err)
 	}
 
+}
+
+func TestNewDefaultTwoSDK(t *testing.T) {
+	setup := Options{
+		ConfigFile: "../../test/fixtures/config/config_test.yaml",
+		StateStoreOpts: opt.StateStoreOpts{
+			Path: "/tmp/state",
+		},
+	}
+
+	sdk1, err := NewSDK(setup)
+	if err != nil {
+		t.Fatalf("Error initializing SDK: %s", err)
+	}
+
+	setup.ConfigFile = "../../test/fixtures/config/config_org2_test.yaml"
+	sdk2, err := NewSDK(setup)
+	if err != nil {
+		t.Fatalf("Error initializing SDK: %s", err)
+	}
+
+	client1, err := sdk1.configProvider.Client()
+	if err != nil {
+		t.Fatalf("Error getting client from config: %s", err)
+	}
+
+	if client1.Organization != "Org1" {
+		t.Fatalf("Unexpected org in config: %s", client1.Organization)
+	}
+
+	client2, err := sdk2.configProvider.Client()
+	if err != nil {
+		t.Fatalf("Error getting client from config: %s", err)
+	}
+
+	if client2.Organization != "Org2" {
+		t.Fatalf("Unexpected org in config: %s", client1.Organization)
+	}
 }
