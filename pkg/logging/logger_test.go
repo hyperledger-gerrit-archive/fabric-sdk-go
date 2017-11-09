69c291042a3dd2c32bb034ef2a838daa5388663b
Sudesh Shetty <sudesh.shetty@securekey.com>
2017-11-09 09:56:04 -0500

    [FAB-6356] enable/disable callerinfo by log level
    for Go-SDK, by default enabled for DEBUG
    disabled for rest
    
    
    Change-Id: I34803a2c94759b4068c7dfbe7fa1ad08f45749db
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- pkg/logging/logger_test.go
+++ pkg/logging/logger_test.go
@@ -112,7 +112,7 @@ func verifyCriticalLoggings(t *testing.T, level apilogging.Level, loggerFunc fn,
 			t.Errorf("%v was supposed to panic", loggerFunc)
 		}
 		var regex string
-		if IsCallerInfoEnabled(moduleName) {
+		if IsCallerInfoEnabled(CRITICAL) {
 			//with caller info
 			regex = fmt.Sprintf(basicLevelOutputWithCallerInfoExpectedRegex, moduleName, levelNames[level])
 		} else {
@@ -152,7 +152,7 @@ func verifyBasicLogging(t *testing.T, level apilogging.Level, loggerFunc fn, log
 		regex = fmt.Sprintf(customLevelOutputExpectedRegex, moduleName)
 	} else if level > 0 && !verifyCustom {
 		levelName = levelNames[level]
-		if IsCallerInfoEnabled(moduleName) {
+		if IsCallerInfoEnabled(level) {
 			//with caller info
 			regex = fmt.Sprintf(basicLevelOutputWithCallerInfoExpectedRegex, moduleName, levelName)
 		} else {
