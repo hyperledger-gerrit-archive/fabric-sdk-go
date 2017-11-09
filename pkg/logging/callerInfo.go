ab686905c7f45fc23c25bbf869bfb24f3de6a8d1
Sudesh Shetty <sudesh.shetty@securekey.com>
2017-11-09 16:05:06 -0500

    [FAB-6356] enable/disable callerinfo by module/loglevel
    For Go-SDK, by default enabled it for all log levels
    for sdk-go log module.
    
    
    Change-Id: I34803a2c94759b4068c7dfbe7fa1ad08f45749db
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- pkg/logging/callerInfo.go
+++ pkg/logging/callerInfo.go
@@ -6,39 +6,61 @@ SPDX-License-Identifier: Apache-2.0
 
 package logging
 
-import "sync"
+import (
+	"sync"
+
+	"github.com/hyperledger/fabric-sdk-go/api/apilogging"
+)
+
+type callerInfoKey struct {
+	module string
+	level  apilogging.Level
+}
 
 type callerInfo struct {
 	sync.RWMutex
-	showcaller map[string]bool
+	showcaller map[callerInfoKey]bool
 }
 
-func (l *callerInfo) ShowCallerInfo(module string) {
-	l.RLock()
-	defer l.RUnlock()
-	if l.showcaller == nil {
-		l.showcaller = make(map[string]bool)
-	}
-	l.showcaller[module] = true
-}
-
-func (l *callerInfo) HideCallerInfo(module string) {
+func (l *callerInfo) ShowCallerInfo(module string, level apilogging.Level) {
 	l.Lock()
 	defer l.Unlock()
 	if l.showcaller == nil {
-		l.showcaller = make(map[string]bool)
+		l.showcaller = l.getDefaultCallerInfoSetting()
 	}
-	l.showcaller[module] = false
+	l.showcaller[callerInfoKey{module, level}] = true
 }
 
-func (l *callerInfo) IsCallerInfoEnabled(module string) bool {
-	showcaller, exists := l.showcaller[module]
+func (l *callerInfo) HideCallerInfo(module string, level apilogging.Level) {
+	l.Lock()
+	defer l.Unlock()
+	if l.showcaller == nil {
+		l.showcaller = l.getDefaultCallerInfoSetting()
+	}
+	l.showcaller[callerInfoKey{module, level}] = false
+}
+
+func (l *callerInfo) IsCallerInfoEnabled(module string, level apilogging.Level) bool {
+	l.RLock()
+	defer l.RUnlock()
+	showcaller, exists := l.showcaller[callerInfoKey{module, level}]
 	if exists == false {
-		showcaller, exists = l.showcaller[""]
-		// no configuration exists, default to false
+		//If no callerinfo setting exists, then look for default
+		showcaller, exists = l.showcaller[callerInfoKey{"", level}]
 		if exists == false {
-			return false
+			return true
 		}
 	}
 	return showcaller
+}
+
+//getDefaultCallerInfoSetting default setting for callerinfo
+func (l *callerInfo) getDefaultCallerInfoSetting() map[callerInfoKey]bool {
+	return map[callerInfoKey]bool{
+		callerInfoKey{"", CRITICAL}: true,
+		callerInfoKey{"", ERROR}:    true,
+		callerInfoKey{"", WARNING}:  true,
+		callerInfoKey{"", INFO}:     true,
+		callerInfoKey{"", DEBUG}:    true,
+	}
 }
