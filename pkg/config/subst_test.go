16797154ba1bee1234bb3fb9205a96ada731fe0c
Troy Ronda <troy@troyronda.com>
2017-12-07 22:42:06 -0500

    [FAB-7392] Enable multiple version testing
    This patch adds new make targets for testing against
    various Fabric code levels. Additionally some fixtures
    are versioned to support the code levels.
    
    Change-Id: I581e2ca94ac52c76599a5599711bf0f7b2b81578
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ pkg/config/subst_test.go
@@ -0,0 +1,112 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package config
+
+import (
+	"testing"
+
+	"github.com/hyperledger/fabric-sdk-go/test/metadata"
+)
+
+func TestSubstCryptoConfigMiddle(t *testing.T) {
+	o := "foo${CRYPTOCONFIGPATH}foo"
+	s := substPathVars(o)
+	e := "foo" + metadata.CryptoConfigPath + "foo"
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstCryptoConfigPrefix(t *testing.T) {
+	o := "foo${CRYPTOCONFIGPATH}"
+	s := substPathVars(o)
+	e := "foo" + metadata.CryptoConfigPath
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+func TestSubstCryptoConfigWithPostfix(t *testing.T) {
+	o := "${CRYPTOCONFIGPATH}foo"
+	s := substPathVars(o)
+	e := metadata.CryptoConfigPath + "foo"
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstCryptoConfigOnly(t *testing.T) {
+	o := "${CRYPTOCONFIGPATH}"
+	s := substPathVars(o)
+	e := metadata.CryptoConfigPath
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstAlmostVar1(t *testing.T) {
+	o := "${FOO}"
+	s := substPathVars(o)
+	e := o
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstAlmostVar2(t *testing.T) {
+	o := "${FOO${}${}$"
+	s := substPathVars(o)
+	e := o
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstNoVar(t *testing.T) {
+	o := "foo"
+	s := substPathVars(o)
+	e := o
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstEmptyVar(t *testing.T) {
+	o := ""
+	s := substPathVars(o)
+	e := o
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstGoPath1(t *testing.T) {
+	o := "foo${GOPATH}foo"
+	s := substPathVars(o)
+	e := "foo" + goPath() + "foo"
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
+
+func TestSubstGoPath2(t *testing.T) {
+	o := "foo$GOPATHfoo"
+	s := substPathVars(o)
+	e := "foo" + goPath() + "foo"
+
+	if s != e {
+		t.Fatalf("Unexpected path substitution (%s, %s)", s, e)
+	}
+}
