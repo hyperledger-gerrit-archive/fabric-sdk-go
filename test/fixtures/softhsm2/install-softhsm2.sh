eec86f780fca2407ef2c14f713377d7fea2cf783
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2018-01-11 08:31:20 -0500

    WIP: Read private key from cert
    Change-Id: I16a3a4fc0275c38d86a85ede90899f0a623151e8
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- test/fixtures/softhsm2/install-softhsm2.sh
+++ test/fixtures/softhsm2/install-softhsm2.sh
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-set -e
+set -xe
 
 ARCH=`uname -m`
 
@@ -13,7 +13,12 @@ if [ $ARCH = "s390x" ]; then
 fi
 
 apt-get update && \
-apt-get -qq install -y make zlib1g-dev libbz2-dev libyaml-dev libltdl-dev libtool curl ca-certificates openssl git softhsm2 && \
+apt-get install -y --no-install-recommends softhsm2 curl git gcc g++ libtool libltdl-dev && \
 mkdir -p /var/lib/softhsm/tokens/ && \
 softhsm2-util --init-token --slot 0 --label "ForFabric" --so-pin 1234 --pin 98765432 && \
-rm -rf /var/lib/apt/lists/*
+mkdir -p ${GOROOT} ${GOPATH} && \
+GO_URL=https://storage.googleapis.com/golang/go${GO_VERSION}.linux-amd64.tar.gz; \
+curl -o /tmp/go.tar.gz ${GO_URL} && \
+tar -xvzf /tmp/go.tar.gz -C /opt/ && \
+rm -rf /tmp/go.tar.gz && \
+go get ${PKCS11_TOOL_URL}
