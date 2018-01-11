621004c14524c45b9d5a68e45501dd68d379dbb3
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2018-01-11 08:31:20 -0500

    WIP: Read private key from cert
    Change-Id: I16a3a4fc0275c38d86a85ede90899f0a623151e8
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- def/fabapi/fabapi.go
+++ def/fabapi/fabapi.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hyperledger/fabric-sdk-go/def/factory/defclient"
 	"github.com/hyperledger/fabric-sdk-go/def/factory/defcore"
 	"github.com/hyperledger/fabric-sdk-go/def/factory/defsvc"
+	"github.com/hyperledger/fabric-sdk-go/pkg/cryptosuite"
 	"github.com/hyperledger/fabric-sdk-go/pkg/errors"
 	"github.com/hyperledger/fabric-sdk-go/pkg/logging"
 
@@ -128,11 +129,15 @@ func NewSDK(options Options) (*FabricSDK, error) {
 	sdk.configProvider = config
 
 	// Initialize crypto provider
-	cryptosuite, err := sdk.CoreFactory.NewCryptoSuiteProvider(sdk.configProvider)
+	cs, err := sdk.CoreFactory.NewCryptoSuiteProvider(sdk.configProvider)
 	if err != nil {
 		return nil, errors.WithMessage(err, "failed to initialize crypto suite")
 	}
-	sdk.cryptoSuite = cryptosuite
+
+	//Setting this cryptosuite as a factory default too
+	cryptosuite.SetDefault(cs)
+
+	sdk.cryptoSuite = cs
 
 	// Initialize state store
 	store, err := sdk.CoreFactory.NewStateStoreProvider(sdk.StateStoreOpts, sdk.configProvider)
