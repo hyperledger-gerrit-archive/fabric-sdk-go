a94d241dbd327b87276eee3c3c1c9bc459a22016
Troy Ronda <troy@troyronda.com>
2018-01-19 14:31:13 -0500

    WIP [FAB-7821] Update tests to use generic client opts
    Change-Id: I9bb77b29f56011b07e16edfcac3eed2c8b86948b
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/fabsdk/deprecated_test.go
+++ pkg/fabsdk/deprecated_test.go
@@ -6,6 +6,7 @@ SPDX-License-Identifier: Apache-2.0
 
 package fabsdk
 
+/*
 import (
 	"testing"
 
@@ -28,6 +29,18 @@ func TestNewChannelMgmtWithOptsClient(t *testing.T) {
 	sdk, err := New(c)
 	if err != nil {
 		t.Fatalf("Error initializing SDK: %s", err)
+	}
+
+	// Test configuration failure for channel management client (invalid user/default organisation)
+	_, err = sdk.NewChannelMgmtClient("Invalid")
+	if err == nil {
+		t.Fatalf("Should have failed to create channel client due to invalid user")
+	}
+
+	// Test valid configuration for channel management client
+	_, err = sdk.NewChannelMgmtClient(clientValidAdmin)
+	if err != nil {
+		t.Fatalf("Failed to create new channel client: %s", err)
 	}
 
 	// Test configuration failure for new channel management client with options (invalid org)
@@ -53,6 +66,18 @@ func TestNewResourceMgmtWithOptsClient(t *testing.T) {
 	sdk, err := New(c)
 	if err != nil {
 		t.Fatalf("Error initializing SDK: %s", err)
+	}
+
+	// Test configuration failure for resource management client (invalid user/default organisation)
+	_, err = sdk.NewResourceMgmtClient("Invalid")
+	if err == nil {
+		t.Fatalf("Should have failed to create resource management client due to invalid user")
+	}
+
+	// Test valid configuration for resource management client
+	_, err = sdk.NewResourceMgmtClient(clientValidAdmin)
+	if err != nil {
+		t.Fatalf("Failed to create new resource management client: %s", err)
 	}
 
 	// Test configuration failure for new resource management client with options (invalid org)
@@ -89,3 +114,4 @@ func TestNewPreEnrolledUserSession(t *testing.T) {
 		t.Fatal("Expected error loading user session from fake org")
 	}
 }
+*/
