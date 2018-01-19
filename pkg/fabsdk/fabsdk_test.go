13407d380146f0eb208d594cbad46762782955b2
Troy Ronda <troy@troyronda.com>
2018-01-18 19:00:13 -0500

    [FAB-7623] Make channel client options generic
    This patch create a new SDK function Client that allows
    SDK consumers to use the client APIs provided by the
    SDK. Previously each type of API was exposed as its own
    function.
    
    Change-Id: I70ffac847c09e689162d6ca23404e5f571c2a871
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/fabsdk/fabsdk_test.go
+++ pkg/fabsdk/fabsdk_test.go
@@ -16,6 +16,7 @@ import (
 	mockapisdk "github.com/hyperledger/fabric-sdk-go/pkg/fabsdk/api/mocks"
 )
 
+/*
 func TestPanicOnNilConfig(t *testing.T) {
 	defer func() {
 		if r := recover(); r == nil {
@@ -25,6 +26,7 @@ func TestPanicOnNilConfig(t *testing.T) {
 
 	New(nil)
 }
+*/
 
 func TestNewGoodOpt(t *testing.T) {
 	c, err := configImpl.FromFile("../../test/fixtures/config/config_test.yaml")
@@ -61,6 +63,7 @@ func badOpt() Option {
 		return errors.New("Bad Opt")
 	}
 }
+
 func TestNewDefaultSDK(t *testing.T) {
 	// Test New SDK with valid config file
 	c, err := configImpl.FromFile("../../test/fixtures/config/config_test.yaml")
@@ -227,10 +230,12 @@ func TestWithSessionPkg(t *testing.T) {
 		t.Fatalf("Unexpected error getting context: %s", err)
 	}
 
-	session, err := sdk.NewPreEnrolledUserSession("Org1", "User1")
+	identity, err := sdk.newIdentity("Org1", FromUserID("User1"))
 	if err != nil {
-		t.Fatalf("Unexpected error getting session: %s", err)
+		t.Fatalf("Unexpected error getting identity: %s", err)
 	}
+
+	session := newSession(identity)
 
 	cm, err := sessPkg.NewChannelMgmtClient(sdk, session, c)
 	if err != nil {
@@ -292,79 +297,6 @@ func TestErrPkgSuite(t *testing.T) {
 		t.Fatalf("Expected error initializing SDK")
 	}
 	ps.errOnLogger = false
-}
-
-func TestNewChannelMgmtClient(t *testing.T) {
-	c, err := configImpl.FromFile("../../test/fixtures/config/config_test.yaml")
-	if err != nil {
-		t.Fatalf("Unexpected error from config: %v", err)
-	}
-
-	sdk, err := New(c)
-	if err != nil {
-		t.Fatalf("Error initializing SDK: %s", err)
-	}
-
-	// Test configuration failure for channel management client (invalid user/default organisation)
-	_, err = sdk.NewChannelMgmtClient("Invalid")
-	if err == nil {
-		t.Fatalf("Should have failed to create channel client due to invalid user")
-	}
-
-	// Test valid configuration for channel management client
-	_, err = sdk.NewChannelMgmtClient("Admin")
-	if err != nil {
-		t.Fatalf("Failed to create new channel client: %s", err)
-	}
-
-	// Test configuration failure for new channel management client with options (invalid org)
-	_, err = sdk.NewChannelMgmtClientWithOpts("Admin", &ChannelMgmtClientOpts{OrgName: "Invalid"})
-	if err == nil {
-		t.Fatalf("Should have failed to create channel client due to invalid organisation")
-	}
-
-	// Test new channel management client with options (orderer admin configuration)
-	_, err = sdk.NewChannelMgmtClientWithOpts("Admin", &ChannelMgmtClientOpts{OrgName: "ordererorg"})
-	if err != nil {
-		t.Fatalf("Failed to create new channel client with opts: %s", err)
-	}
-
-}
-
-func TestNewResourceMgmtClient(t *testing.T) {
-	c, err := configImpl.FromFile("../../test/fixtures/config/config_test.yaml")
-	if err != nil {
-		t.Fatalf("Unexpected error from config: %v", err)
-	}
-
-	sdk, err := New(c)
-	if err != nil {
-		t.Fatalf("Error initializing SDK: %s", err)
-	}
-
-	// Test configuration failure for resource management client (invalid user/default organisation)
-	_, err = sdk.NewResourceMgmtClient("Invalid")
-	if err == nil {
-		t.Fatalf("Should have failed to create resource management client due to invalid user")
-	}
-
-	// Test valid configuration for resource management client
-	_, err = sdk.NewResourceMgmtClient("Admin")
-	if err != nil {
-		t.Fatalf("Failed to create new resource management client: %s", err)
-	}
-
-	// Test configuration failure for new resource management client with options (invalid org)
-	_, err = sdk.NewResourceMgmtClientWithOpts("Admin", &ResourceMgmtClientOpts{OrgName: "Invalid"})
-	if err == nil {
-		t.Fatalf("Should have failed to create resource management client due to invalid organization")
-	}
-
-	// Test new resource management client with options (Org2 configuration)
-	_, err = sdk.NewResourceMgmtClientWithOpts("Admin", &ResourceMgmtClientOpts{OrgName: "Org2"})
-	if err != nil {
-		t.Fatalf("Failed to create new resource management client with opts: %s", err)
-	}
 }
 
 func TestNewDefaultTwoValidSDK(t *testing.T) {
---
+++ pkg/fabsdk/session_test.go
@@ -0,0 +1,90 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package fabsdk
+
+import (
+	"testing"
+
+	configImpl "github.com/hyperledger/fabric-sdk-go/pkg/config"
+)
+
+const (
+	identityOptConfigFile = "testdata/test.yaml"
+	validIdentityOptUser  = "user1"
+	validIdentityOptOrg   = "Org2"
+)
+
+func TestFromUserIDValid(t *testing.T) {
+	c, err := configImpl.FromFile(identityOptConfigFile)
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	opts := identityOptions{}
+	opt := FromUserID(validIdentityOptUser)
+	err = opt(&opts, sdk, validIdentityOptOrg)
+	if err != nil {
+		t.Fatalf("Expected no error from opt, but got %v", err)
+	}
+	if opts.identity == nil {
+		t.Fatal("Expected identity to be populated")
+	}
+}
+
+func TestFromUserIDInvalid(t *testing.T) {
+	c, err := configImpl.FromFile(identityOptConfigFile)
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	opts := identityOptions{}
+	opt := FromUserID("notarealuser")
+	err = opt(&opts, sdk, validIdentityOptOrg)
+	if err == nil {
+		t.Fatal("Expected error from opt")
+	}
+	if opts.identity != nil {
+		t.Fatal("Expected identity to not be populated")
+	}
+}
+
+func TestFromIdentity(t *testing.T) {
+	c, err := configImpl.FromFile(identityOptConfigFile)
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	identity, err := sdk.NewPreEnrolledUser(validIdentityOptOrg, validIdentityOptUser)
+	if err != nil {
+		t.Fatalf("Unexpected error from loading user: %v", err)
+	}
+
+	opts := identityOptions{}
+	opt := fromIdentity(identity)
+	err = opt(&opts, sdk, validIdentityOptOrg)
+	if err != nil {
+		t.Fatalf("Expected no error from opt, but got %v", err)
+	}
+	if opts.identity != identity {
+		t.Fatal("Expected identity to be populated")
+	}
+}
