795a680339f121ae0287344d8d17c8d89aa94402
Troy Ronda <troy@troyronda.com>
2018-01-18 19:00:13 -0500

    [FAB-7623] Make channel client options generic
    This patch create a new SDK function Client that allows
    SDK consumers to use the client APIs provided by the
    SDK. Previously each type of API was exposed as its own
    function.
    
    Change-Id: I70ffac847c09e689162d6ca23404e5f571c2a871
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ pkg/fabsdk/client_test.go
@@ -0,0 +1,145 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package fabsdk
+
+import (
+	"testing"
+
+	"github.com/hyperledger/fabric-sdk-go/api/apifabclient"
+	configImpl "github.com/hyperledger/fabric-sdk-go/pkg/config"
+	"github.com/hyperledger/fabric-sdk-go/pkg/errors"
+)
+
+func TestNewGoodClientOpt(t *testing.T) {
+	c, err := configImpl.FromFile("testdata/test.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	_, err = sdk.Client(FromUserID("user1"), goodClientOpt())
+	if err != nil {
+		t.Fatalf("Expected no error from Client, but got %v", err)
+	}
+}
+
+func goodClientOpt() ClientOption {
+	return func(opts *clientOptions) error {
+		return nil
+	}
+}
+
+func TestNewBadClientOpt(t *testing.T) {
+	c, err := configImpl.FromFile("testdata/test.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	_, err = sdk.Client(FromUserID("user1"), badClientOpt())
+	if err == nil {
+		t.Fatal("Expected error from Client")
+	}
+}
+
+func badClientOpt() ClientOption {
+	return func(opts *clientOptions) error {
+		return errors.New("Bad Opt")
+	}
+}
+
+func TestWithOrg(t *testing.T) {
+	c, err := configImpl.FromFile("testdata/test.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	_, err = sdk.Client(FromUserID("notarealuser"), WithOrg("orgX"))
+	if err == nil {
+		t.Fatal("Expected error from Client")
+	}
+
+	_, err = sdk.Client(FromUserID("orgXuser"), WithOrg("orgX"))
+	if err != nil {
+		t.Fatalf("Expected no error from Client, but got %v", err)
+	}
+}
+
+func TestWithFilter(t *testing.T) {
+	tf := mockTargetFilter{}
+	opt := WithTargetFilter(&tf)
+
+	opts := clientOptions{}
+	err := opt(&opts)
+	if err != nil {
+		t.Fatalf("Expected no error from option, but got %v", err)
+	}
+
+	if opts.targetFilter != &tf {
+		t.Fatalf("Expected target filter to be set in opts")
+	}
+}
+
+func TestWithConfig(t *testing.T) {
+	c, err := configImpl.FromFile("testdata/test.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+	opt := withConfig(c)
+
+	opts := clientOptions{}
+	err = opt(&opts)
+	if err != nil {
+		t.Fatalf("Expected no error from option, but got %v", err)
+	}
+
+	if opts.configProvider != c {
+		t.Fatalf("Expected config to be set in opts")
+	}
+}
+
+func TestNoIdentity(t *testing.T) {
+	c, err := configImpl.FromFile("testdata/test.yaml")
+	if err != nil {
+		t.Fatalf("Unexpected error from config: %v", err)
+	}
+
+	sdk, err := New(c)
+	if err != nil {
+		t.Fatalf("Expected no error from New, but got %v", err)
+	}
+
+	_, err = sdk.Client(noopIdentityOpt(), goodClientOpt())
+	if err == nil {
+		t.Fatal("Expected error from Client")
+	}
+}
+
+func noopIdentityOpt() IdentityOption {
+	return func(o *identityOptions, sdk *FabricSDK, orgName string) error {
+		return nil
+	}
+}
+
+type mockTargetFilter struct{}
+
+func (f *mockTargetFilter) Accept(peer apifabclient.Peer) bool {
+	return false
+}
