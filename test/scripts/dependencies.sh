40fea3f5a4b7515a85df04779bb2bed049be97bd
Troy Ronda <troy@troyronda.com>
2018-01-31 20:30:28 -0500

    [FAB-8005] Fix CI
    Change-Id: If2e07fce7ffde4fc424afa047cfc3edaf6d29e15
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/scripts/dependencies.sh
+++ test/scripts/dependencies.sh
@@ -16,12 +16,11 @@ GOPATH="${GOPATH:-$HOME/go}"
 # Automatically install go tools (particularly for CI)
 if [ "$FABRIC_SDKGO_DEPEND_INSTALL" = "true" ]; then
     echo "Installing dependencies ..."
-    $GO_CMD get -u github.com/axw/gocov/...
-    $GO_CMD get -u github.com/AlekSi/gocov-xml
-    $GO_CMD get -u github.com/client9/misspell/cmd/misspell
-    $GO_CMD get -u github.com/golang/lint/golint
-    $GO_CMD get -u golang.org/x/tools/cmd/goimports
-    $GO_CMD get -u github.com/golang/mock/mockgen
+    $GO_CMD get github.com/axw/gocov/...
+    $GO_CMD get github.com/AlekSi/gocov-xml
+    $GO_CMD get github.com/client9/misspell/cmd/misspell
+    $GO_CMD get github.com/golang/lint/golint
+    $GO_CMD get golang.org/x/tools/cmd/goimports
 fi
 
 # Install specific version of go dep (particularly for CI)
@@ -46,6 +45,9 @@ if [ "$FABRIC_SDKGO_DEPEND_INSTALL" = "true" ] && [ -n "$GO_MOCKGEN_COMMIT" ]; t
     (cd $TMP/src/github.com/golang/mock && git reset --hard $GO_MOCKGEN_COMMIT)
     GOPATH=$TMP go install github.com/golang/mock/mockgen
     cp $TMP/bin/mockgen $GOPATH/bin
+
+    mkdir -p $GOPATH/src/github.com/golang/mock
+    cp -R $TMP/src/github.com/golang/mock/mockgen $GOPATH/src/github.com/golang/mock
 
     rm -Rf $TMP
 fi
