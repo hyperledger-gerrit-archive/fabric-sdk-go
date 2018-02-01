b2ce8c304946f9bbcab4e95a05876bd9fd26beda
Troy Ronda <troy@troyronda.com>
2018-01-31 20:30:28 -0500

    [FAB-8005] Fix CI
    Change-Id: I81bd424b4f769d4f7ce032984075971db6b4a453
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/scripts/dependencies.sh
+++ test/scripts/dependencies.sh
@@ -16,12 +16,16 @@ GOPATH="${GOPATH:-$HOME/go}"
 # Automatically install go tools (particularly for CI)
 if [ "$FABRIC_SDKGO_DEPEND_INSTALL" = "true" ]; then
     echo "Installing dependencies ..."
-    $GO_CMD get -u github.com/axw/gocov/...
-    $GO_CMD get -u github.com/AlekSi/gocov-xml
-    $GO_CMD get -u github.com/client9/misspell/cmd/misspell
-    $GO_CMD get -u github.com/golang/lint/golint
-    $GO_CMD get -u golang.org/x/tools/cmd/goimports
-    $GO_CMD get -u github.com/golang/mock/mockgen
+    TMP=`mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir'`
+    GOPATH=$TMP $GO_CMD get -v -u github.com/axw/gocov/...
+    GOPATH=$TMP $GO_CMD get -v -u github.com/AlekSi/gocov-xml
+    cp $TMP/bin/gocov* $GOPATH/bin
+    GOPATH=$TMP $GO_CMD get -v -u github.com/client9/misspell/cmd/misspell
+    cp $TMP/bin/misspell $GOPATH/bin
+    GOPATH=$TMP $GO_CMD get -v -u github.com/golang/lint/golint
+    cp $TMP/bin/golint $GOPATH/bin
+    GOPATH=$TMP $GO_CMD get -v -u golang.org/x/tools/cmd/goimports
+    cp $TMP/bin/goimports $GOPATH/bin
 fi
 
 # Install specific version of go dep (particularly for CI)
