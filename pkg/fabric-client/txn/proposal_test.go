604cfb78f29ffd1a183d9d5176199b02cf978989
Troy Ronda <troy@troyronda.com>
2018-02-15 18:45:13 -0500

    [FAB-8054] WIP Split transactor from Channel
    Change-Id: I07409a4ef58ae9e39fa38850738cb0a53000e6e0
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/fabric-client/txn/proposal_test.go
+++ pkg/fabric-client/txn/proposal_test.go
@@ -31,7 +31,7 @@ func TestNewTransactionProposal(t *testing.T) {
 	user := mocks.NewMockUserWithMSPID("test", "1234")
 	ctx := mocks.NewMockContext(user)
 
-	request := fab.ChaincodeInvokeRequest{
+	request := fab.TransactionProposalRequest{
 		ChaincodeID: "qscc",
 		Fcn:         "Hello",
 	}
@@ -41,12 +41,15 @@ func TestNewTransactionProposal(t *testing.T) {
 		t.Fatalf("Create Transaction Proposal Failed: %s", err)
 	}
 
-	_, err = proto.Marshal(tp.SignedProposal)
+	signedProposal, err := signProposal(ctx, tp.Proposal)
+	if err != nil {
+		t.Fatalf("signProposal failed: %s", err)
+	}
 
+	_, err = proto.Marshal(signedProposal)
 	if err != nil {
 		t.Fatalf("Call to proposal bytes failed: %s", err)
 	}
-
 }
 
 func TestSendTransactionProposal(t *testing.T) {
@@ -58,7 +61,7 @@ func TestSendTransactionProposal(t *testing.T) {
 		MockRoles: []string{}, MockCert: nil, Status: 200, Payload: []byte("A"),
 		ResponseMessage: responseMessage}
 
-	request := fab.ChaincodeInvokeRequest{
+	request := fab.TransactionProposalRequest{
 		ChaincodeID: "cc",
 		Fcn:         "Hello",
 		Args:        [][]byte{[]byte{1, 2, 3}},
@@ -69,7 +72,7 @@ func TestSendTransactionProposal(t *testing.T) {
 		t.Fatalf("new transaction proposal failed: %s", err)
 	}
 
-	tpr, err := SendProposal(tp, []fab.ProposalProcessor{&peer})
+	tpr, err := SendProposal(ctx, tp, []fab.ProposalProcessor{&peer})
 	if err != nil {
 		t.Fatalf("send transaction proposal failed: %s", err)
 	}
@@ -85,7 +88,7 @@ func TestNewTransactionProposalParams(t *testing.T) {
 	user := mocks.NewMockUserWithMSPID("test", "1234")
 	ctx := mocks.NewMockContext(user)
 
-	request := fab.ChaincodeInvokeRequest{
+	request := fab.TransactionProposalRequest{
 		ChaincodeID: "cc",
 		Fcn:         "Hello",
 	}
@@ -95,12 +98,12 @@ func TestNewTransactionProposalParams(t *testing.T) {
 		t.Fatalf("new transaction proposal failed: %s", err)
 	}
 
-	_, err = SendProposal(tp, nil)
+	_, err = SendProposal(ctx, tp, nil)
 	if err == nil {
 		t.Fatalf("Expected error")
 	}
 
-	request = fab.ChaincodeInvokeRequest{
+	request = fab.TransactionProposalRequest{
 		Fcn: "Hello",
 	}
 
@@ -109,7 +112,7 @@ func TestNewTransactionProposalParams(t *testing.T) {
 		t.Fatalf("Expected error")
 	}
 
-	request = fab.ChaincodeInvokeRequest{
+	request = fab.TransactionProposalRequest{
 		ChaincodeID: "cc",
 	}
 
@@ -118,7 +121,7 @@ func TestNewTransactionProposalParams(t *testing.T) {
 		t.Fatalf("Expected error")
 	}
 
-	request = fab.ChaincodeInvokeRequest{
+	request = fab.TransactionProposalRequest{
 		ChaincodeID: "cc",
 		Fcn:         "Hello",
 	}
@@ -132,8 +135,11 @@ func TestConcurrentPeers(t *testing.T) {
 	const numPeers = 10000
 	peers := setupMassiveTestPeers(numPeers)
 
-	result, err := SendProposal(&fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	user := mocks.NewMockUserWithMSPID("test", "1234")
+	ctx := mocks.NewMockContext(user)
+
+	result, err := SendProposal(ctx, &fab.TransactionProposal{
+		Proposal: &pb.Proposal{},
 	}, peers)
 	if err != nil {
 		t.Fatalf("SendProposal return error: %s", err)
@@ -142,55 +148,47 @@ func TestConcurrentPeers(t *testing.T) {
 	if len(result) != numPeers {
 		t.Error("SendTransactionProposal returned an unexpected amount of responses")
 	}
-
-	//Negative scenarios
-	_, err = SendProposal(nil, nil)
-
-	if err == nil || err.Error() != "signedProposal is required" {
-		t.Fatal("nil signedProposal validation check not working as expected")
-	}
 }
 
 func TestSendTransactionProposalToProcessors(t *testing.T) {
+
+	user := mocks.NewMockUserWithMSPID("test", "1234")
+	ctx := mocks.NewMockContext(user)
 
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	proc := mock_apifabclient.NewMockProposalProcessor(mockCtrl)
 
-	tp := fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	stp, err := signProposal(ctx, &pb.Proposal{})
+	if err != nil {
+		t.Fatalf("signProposal returned error: %s", err)
 	}
-	tpr := fab.TransactionProposalResponse{Endorser: "example.com", Status: 99, Proposal: tp, ProposalResponse: nil}
-	proc.EXPECT().ProcessTransactionProposal(tp).Return(tpr, nil)
+	tp := fab.ProcessProposalRequest{
+		SignedProposal: stp,
+	}
+
+	tpr := fab.TransactionProposalResponse{Endorser: "example.com", Status: 99}
+	proc.EXPECT().ProcessTransactionProposal(tp).Return(&tpr, nil)
 	targets := []fab.ProposalProcessor{proc}
 
-	result, err := SendProposal(&fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	result, err := SendProposal(ctx, &fab.TransactionProposal{
+		Proposal: &pb.Proposal{},
 	}, nil)
 
 	if result != nil || err == nil || err.Error() != "targets is required" {
 		t.Fatalf("Test SendTransactionProposal failed, validation on peer is nil is not working as expected: %v", err)
 	}
 
-	result, err = SendProposal(&fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	result, err = SendProposal(ctx, &fab.TransactionProposal{
+		Proposal: &pb.Proposal{},
 	}, []fab.ProposalProcessor{})
 
 	if result != nil || err == nil || err.Error() != "targets is required" {
 		t.Fatalf("Test SendTransactionProposal failed, validation on missing peer objects is not working: %v", err)
 	}
 
-	result, err = SendProposal(&fab.TransactionProposal{
-		SignedProposal: nil,
-	}, nil)
-
-	if result != nil || err == nil || err.Error() != "signedProposal is required" {
-		t.Fatal("Test SendTransactionProposal failed, validation on signedProposal is nil is not working as expected")
-	}
-
-	result, err = SendProposal(&fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
-	}, targets)
+	result, err = SendProposal(ctx, &fab.TransactionProposal{
+		Proposal: &pb.Proposal{}}, targets)
 
 	if result == nil || err != nil {
 		t.Fatalf("Test SendTransactionProposal failed, with error '%s'", err.Error())
@@ -200,24 +198,30 @@ func TestSendTransactionProposalToProcessors(t *testing.T) {
 func TestProposalResponseError(t *testing.T) {
 	testError := fmt.Errorf("Test Error")
 
+	user := mocks.NewMockUserWithMSPID("test", "1234")
+	ctx := mocks.NewMockContext(user)
+
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 	proc := mock_apifabclient.NewMockProposalProcessor(mockCtrl)
 	proc2 := mock_apifabclient.NewMockProposalProcessor(mockCtrl)
 
-	tp := fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	stp, err := signProposal(ctx, &pb.Proposal{})
+	if err != nil {
+		t.Fatalf("signProposal returned error: %s", err)
+	}
+	tp := fab.ProcessProposalRequest{
+		SignedProposal: stp,
 	}
 
 	// Test with error from lower layer
-	tpr := fab.TransactionProposalResponse{Endorser: "example.com", Status: 200,
-		Proposal: tp, ProposalResponse: nil}
-	proc.EXPECT().ProcessTransactionProposal(tp).Return(tpr, testError)
-	proc2.EXPECT().ProcessTransactionProposal(tp).Return(tpr, testError)
+	tpr := fab.TransactionProposalResponse{Endorser: "example.com", Status: 200}
+	proc.EXPECT().ProcessTransactionProposal(tp).Return(&tpr, testError)
+	proc2.EXPECT().ProcessTransactionProposal(tp).Return(&tpr, testError)
 
 	targets := []fab.ProposalProcessor{proc, proc2}
-	_, err := SendProposal(&fab.TransactionProposal{
-		SignedProposal: &pb.SignedProposal{},
+	_, err = SendProposal(ctx, &fab.TransactionProposal{
+		Proposal: &pb.Proposal{},
 	}, targets)
 	errs, ok := err.(multi.Errors)
 	assert.True(t, ok, "expected multi errors object")
