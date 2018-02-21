00ab56643b66c8779d5dfd2823bfe692bcc42899
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2018-02-21 11:46:01 -0500

    [FAB-8395] Abstract Event Service
    Defined an Event Service API and provided
    an abstract implementation which may be
    reused by protocol-specific implementations
    (e.g. Event Hub and Deliver Service)
    
    Change-Id: Ie8b901dfe08df20b01a38ba26c85a3a87f667a41
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

---
+++ pkg/fabric-client/events/service/opts.go
@@ -0,0 +1,21 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package service
+
+type params struct {
+	eventConsumerBufferSize uint
+}
+
+func defaultParams() *params {
+	return &params{
+		eventConsumerBufferSize: 100,
+	}
+}
+
+func (p *params) SetEventConsumerBufferSize(value uint) {
+	p.eventConsumerBufferSize = value
+}
