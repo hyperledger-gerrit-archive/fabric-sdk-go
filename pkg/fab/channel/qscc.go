1d48da1b12461d302346a50c4acf3272b5c302e0
Troy Ronda <troy@troyronda.com>
2018-02-25 20:40:53 -0500

    [FAB-8525] Refactor scc chaincode invocation
    This change extracts invocation request creation
    for qscc, cscc and lscc to their own funcs and files.
    
    Change-Id: I8f2f78df19b5ecb48b2c70cb31b9b31acb4c548b
    Signed-off-by: Troy Ronda <troy@troyronda.com>

---
+++ pkg/fab/channel/qscc.go
@@ -0,0 +1,73 @@
+/*
+Copyright SecureKey Technologies Inc. All Rights Reserved.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package channel
+
+import (
+	"strconv"
+
+	"github.com/hyperledger/fabric-sdk-go/pkg/context/api/fab"
+)
+
+const (
+	qscc                = "qscc"
+	qsccTransactionByID = "GetTransactionByID"
+	qsccChainInfo       = "GetChainInfo"
+	qsccBlockByHash     = "GetBlockByHash"
+	qsccBlockByNumber   = "GetBlockByNumber"
+)
+
+func createTransactionByIDInvokeRequest(channelID string, transactionID fab.TransactionID) fab.ChaincodeInvokeRequest {
+	var args [][]byte
+	args = append(args, []byte(channelID))
+	args = append(args, []byte(transactionID))
+
+	cir := fab.ChaincodeInvokeRequest{
+		ChaincodeID: qscc,
+		Fcn:         qsccTransactionByID,
+	}
+	return cir
+}
+
+func createChainInfoInvokeRequest(channelID string) fab.ChaincodeInvokeRequest {
+	var args [][]byte
+	args = append(args, []byte(channelID))
+
+	cir := fab.ChaincodeInvokeRequest{
+		ChaincodeID: qscc,
+		Fcn:         qsccChainInfo,
+		Args:        args,
+	}
+	return cir
+}
+
+func createBlockByHashInvokeRequest(channelID string, blockHash []byte) fab.ChaincodeInvokeRequest {
+
+	var args [][]byte
+	args = append(args, []byte(channelID))
+	args = append(args, blockHash[:len(blockHash)])
+
+	cir := fab.ChaincodeInvokeRequest{
+		ChaincodeID: qscc,
+		Fcn:         qsccBlockByHash,
+		Args:        args,
+	}
+	return cir
+}
+
+func createBlockByNumberInvokeRequest(channelID string, blockNumber int) fab.ChaincodeInvokeRequest {
+
+	var args [][]byte
+	args = append(args, []byte(channelID))
+	args = append(args, []byte(strconv.Itoa(blockNumber)))
+
+	cir := fab.ChaincodeInvokeRequest{
+		ChaincodeID: qscc,
+		Fcn:         qsccBlockByNumber,
+		Args:        args,
+	}
+	return cir
+}
