4f6a95b12dfffc4dcdc9e853d647f7d7376f492a
Troy Ronda <troy@troyronda.com>
2018-03-05 16:31:44 -0500

    [FAB-8667] Levels in logging pkg
    This change duplicates levels into the logging package, so
    that only one package needs to be imported to set levels.
    
    Change-Id: Ic97de6921e7979e2197906ea813cfa009012c86a
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -25,6 +25,18 @@ type Logger struct {
 var loggerProviderInstance api.LoggerProvider
 var loggerProviderOnce sync.Once
 
+// Level defines all available log levels for log messages.
+type Level int
+
+// Log levels.
+const (
+	CRITICAL Level = iota
+	ERROR
+	WARNING
+	INFO
+	DEBUG
+)
+
 // TODO: enable leveler to redirect to loggerProvider
 //var levelerProvider apilogging.Leveler
 
@@ -77,23 +89,24 @@ func InitLogger(l api.LoggerProvider) {
 }
 
 //SetLevel - setting log level for given module
-func SetLevel(module string, level loglevel.Level) {
-	modlog.SetLevel(module, level)
+func SetLevel(module string, level Level) {
+	modlog.SetLevel(module, loglevel.Level(level))
 }
 
 //GetLevel - getting log level for given module
-func GetLevel(module string) loglevel.Level {
-	return modlog.GetLevel(module)
+func GetLevel(module string) Level {
+	return Level(modlog.GetLevel(module))
 }
 
 //IsEnabledFor - Check if given log level is enabled for given module
-func IsEnabledFor(module string, level loglevel.Level) bool {
-	return modlog.IsEnabledFor(module, level)
+func IsEnabledFor(module string, level Level) bool {
+	return modlog.IsEnabledFor(module, loglevel.Level(level))
 }
 
 // LogLevel returns the log level from a string representation.
-func LogLevel(level string) (loglevel.Level, error) {
-	return loglevel.ParseLevel(level)
+func LogLevel(level string) (Level, error) {
+	l, err := loglevel.ParseLevel(level)
+	return Level(l), err
 }
 
 //Fatal calls Fatal function of underlying logger
@@ -206,4 +219,15 @@ func (l *Logger) logger() api.Logger {
 		l.instance = loggerProvider().GetLogger(l.module)
 	})
 	return l.instance
+}
+
+// ParseLevel returns the log level from a string representation.
+func ParseLevel(level string) (Level, error) {
+	l, err := loglevel.ParseLevel(level)
+	return Level(l), err
+}
+
+//ParseString returns String repressentation of given log level
+func ParseString(level Level) string {
+	return loglevel.ParseString(loglevel.Level(level))
 }
