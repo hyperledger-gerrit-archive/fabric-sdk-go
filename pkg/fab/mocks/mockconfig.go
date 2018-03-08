64830a6f9d131510e82f8cf57f8bd26f84fd6d2b
Baha Shaaban <baha.shaaban@securekey.com>
2018-03-08 18:09:55 -0500

    [FAB-8141] moved out impl from api
    	this was done for code found in
    	fabric-sdk-go/pkg/context/api/core/network.go
    
    Change-Id: I179589a09d9ebc320d2a02558d250a4249f97808
    Signed-off-by: Baha Shaaban <baha.shaaban@securekey.com>

--- pkg/fab/mocks/mockconfig.go
+++ pkg/fab/mocks/mockconfig.go
@@ -12,6 +12,7 @@ import (
 	"time"
 
 	config "github.com/hyperledger/fabric-sdk-go/pkg/context/api/core"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/config/urlutil"
 
 	"github.com/pkg/errors"
 )
@@ -45,11 +46,11 @@ func (c *MockConfig) Client() (*config.ClientConfig, error) {
 		mutualTLSCerts := config.MutualTLSConfig{
 
 			Client: config.TLSKeyPair{
-				Key: config.TLSConfig{
+				Key: urlutil.TLSConfig{
 					Path: "../../../test/fixtures/config/mutual_tls/client_sdk_go-key.pem",
 					Pem:  "",
 				},
-				Cert: config.TLSConfig{
+				Cert: urlutil.TLSConfig{
 					Path: "../../../test/fixtures/config/mutual_tls/client_sdk_go.pem",
 					Pem:  "",
 				},
