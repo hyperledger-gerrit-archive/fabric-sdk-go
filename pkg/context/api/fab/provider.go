c8c18562136ecdbcbf32853057ff007c3ecef05a
Troy Ronda <troy@troyronda.com>
2018-03-08 09:20:44 -0500

    [FAB-8719] Rename New Selection/Discovery to Create
    Change-Id: Ideff2e816fa58edb1c3f4b9cddc5fe753d1f2a8e
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/context/api/fab/provider.go
+++ pkg/context/api/fab/provider.go
@@ -28,7 +28,7 @@ type InfraProvider interface {
 
 // SelectionProvider is used to select peers for endorsement
 type SelectionProvider interface {
-	NewSelectionService(channelID string) (SelectionService, error)
+	CreateSelectionService(channelID string) (SelectionService, error)
 }
 
 // SelectionService selects peers for endorsement and commit events
@@ -40,7 +40,7 @@ type SelectionService interface {
 
 // DiscoveryProvider is used to discover peers on the network
 type DiscoveryProvider interface {
-	NewDiscoveryService(channelID string) (DiscoveryService, error)
+	CreateDiscoveryService(channelID string) (DiscoveryService, error)
 }
 
 // DiscoveryService is used to discover eligible peers on specific channel
