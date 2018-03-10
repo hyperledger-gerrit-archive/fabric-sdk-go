d1b8a523076b533b4bd36ca89f6a22a4836799e8
Troy Ronda <troy@troyronda.com>
2018-03-10 09:35:27 -0500

    [FAB-8767] Move targets into its own argument
    Change-Id: I834fc58ca6b91b6d0981ef40deb982b2ed28f0a2
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/fab/resource/resource_test.go
+++ pkg/fab/resource/resource_test.go
@@ -102,7 +102,7 @@ func TestCreateChannel(t *testing.T) {
 		t.Fatalf("Expected broadcast")
 	}
 }
-
+ 
 func TestJoinChannel(t *testing.T) {
 	var peers []fab.ProposalProcessor
 
@@ -123,16 +123,14 @@ func TestJoinChannel(t *testing.T) {
 	genesisBlock := mocks.NewSimpleMockBlock()
 
 	request := api.JoinChannelRequest{
-		Targets: peers,
 		//GenesisBlock: genesisBlock,
 	}
-	err := JoinChannel(ctx, request)
+	err := JoinChannel(ctx, request, peers)
 	if err == nil {
 		t.Fatalf("Should not have been able to join channel because of missing GenesisBlock parameter")
 	}
 
 	request = api.JoinChannelRequest{
-		Targets:      peers,
 		GenesisBlock: genesisBlock,
 	}
 	if err == nil {
@@ -140,17 +138,15 @@ func TestJoinChannel(t *testing.T) {
 	}
 
 	// Test join channel with valid arguments
-	err = JoinChannel(ctx, request)
+	err = JoinChannel(ctx, request, peers)
 	if err != nil {
 		t.Fatalf("Did not expect error from join channel. Got: %s", err)
 	}
 
 	// Test failed proposal error handling
 	endorserServer.ProposalError = errors.New("Test Error")
-	request = api.JoinChannelRequest{
-		Targets: peers,
-	}
-	err = JoinChannel(ctx, request)
+	request = api.JoinChannelRequest{}
+	err = JoinChannel(ctx, request, peers)
 	if err == nil {
 		t.Fatalf("Expected error")
 	}
