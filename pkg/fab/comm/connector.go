90ecf85887c321ecc72c9295e3da769e2b223a17
biljana lukovic <biljana.lukovic@securekey.com>
2018-03-19 15:31:52 -0400

    [FAB-8964]Log Warning instead of panic
    Change-Id: Id07a3c11d90e2a1cbb46314f6ad24e5b33bc4b32
    Signed-off-by: biljana lukovic <biljana.lukovic@securekey.com>

--- pkg/fab/comm/connector.go
+++ pkg/fab/comm/connector.go
@@ -76,23 +76,25 @@ func (cc *CachingConnector) Close() {
 	cc.lock.Lock()
 	defer cc.lock.Unlock()
 
-	if cc.janitorDone != nil {
-		logger.Debug("closing caching GRPC connector")
-
-		select {
-		case <-cc.janitorClosed:
-			logger.Debugf("janitor not running")
-		default:
-			logger.Debugf("janitor running")
-			cc.janitorDone <- true
-			cc.waitgroup.Wait()
-		}
-
-		close(cc.janitorChan)
-		close(cc.janitorClosed)
-		close(cc.janitorDone)
-		cc.janitorDone = nil
+	if cc.janitorDone == nil {
+		logger.Warn("Trying to close connector after already closed")
+		return
 	}
+	logger.Debug("closing caching GRPC connector")
+
+	select {
+	case <-cc.janitorClosed:
+		logger.Debugf("janitor not running")
+	default:
+		logger.Debugf("janitor running")
+		cc.janitorDone <- true
+		cc.waitgroup.Wait()
+	}
+
+	close(cc.janitorChan)
+	close(cc.janitorClosed)
+	close(cc.janitorDone)
+	cc.janitorDone = nil
 }
 
 // DialContext is a wrapper for grpc.DialContext where connections are cached.
@@ -118,7 +120,14 @@ func (cc *CachingConnector) DialContext(ctx context.Context, target string, opts
 func (cc *CachingConnector) ReleaseConn(conn *grpc.ClientConn) {
 	cc.lock.Lock()
 	defer cc.lock.Unlock()
-
+	if cc.janitorChan == nil {
+		logger.Warn("Trying to close connector after already closed")
+		return
+	}
+	if conn.GetState() == connectivity.Shutdown {
+		logger.Warn("Trying to release connection in shutdown state")
+		return
+	}
 	cconn, ok := cc.index[conn]
 	if !ok {
 		logger.Warnf("connection not found [%p]", conn)
