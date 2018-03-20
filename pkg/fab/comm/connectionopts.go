267e3cff6e794346bfdcefd930deb3459672ab51
Troy Ronda <troy@troyronda.com>
2018-03-20 10:24:53 -0400

    [FAB-8982] Remove binary output in debug log
    Change-Id: Ic20aa429667023ffbff28dad76fa42b8529a21ac
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- pkg/fab/comm/connectionopts.go
+++ pkg/fab/comm/connectionopts.go
@@ -91,7 +91,7 @@ func (p *params) SetHostOverride(value string) {
 }
 
 func (p *params) SetCertificate(value *x509.Certificate) {
-	logger.Debugf("Certificate: %s", value)
+	logger.Debugf("setting certificate [subject: %s, serial: %s]", value.Subject, value.SerialNumber)
 	p.certificate = value
 }
 
