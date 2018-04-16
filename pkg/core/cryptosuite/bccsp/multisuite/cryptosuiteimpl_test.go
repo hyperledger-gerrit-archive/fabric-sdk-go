e357b9b7bb5d83833d87ab41409d26e33cbdd495
gbolo <george.bolo@gmail.com>
2018-04-16 16:10:59 -0400

    [FAB-9540] bccsp security provider type
    allow bccsp security provider type to
    ignore case for string value
    
    Change-Id: Idedf4640823816a4f38f53540486b953ddd69c6a
    Signed-off-by: gbolo <george.bolo@gmail.com>

--- pkg/core/cryptosuite/bccsp/multisuite/cryptosuiteimpl_test.go
+++ pkg/core/cryptosuite/bccsp/multisuite/cryptosuiteimpl_test.go
@@ -33,53 +33,82 @@ func TestBadConfig(t *testing.T) {
 }
 
 func TestCryptoSuiteByConfigSW(t *testing.T) {
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
 
-	mockConfig := mockcore.NewMockCryptoSuiteConfig(mockCtrl)
-	mockConfig.EXPECT().SecurityProvider().Return("SW")
-	mockConfig.EXPECT().SecurityProvider().Return("SW")
-	mockConfig.EXPECT().SecurityAlgorithm().Return("SHA2")
-	mockConfig.EXPECT().SecurityLevel().Return(256)
-	mockConfig.EXPECT().KeyStorePath().Return("")
-	mockConfig.EXPECT().Ephemeral().Return(true)
-
-	//Get cryptosuite using config
-	c, err := GetSuiteByConfig(mockConfig)
-	if err != nil {
-		t.Fatalf("Not supposed to get error, but got: %v", err)
+	// test multiple string cases
+	securityProviderTypes := []string{
+		// upper case
+		"SW",
+		// lower case
+		"sw",
+		// mixed case
+		"Sw",
 	}
 
-	verifySuiteType(t, c, "*sw.impl")
+	for _, securityProviderType := range securityProviderTypes {
+
+		mockCtrl := gomock.NewController(t)
+
+		mockConfig := mockcore.NewMockCryptoSuiteConfig(mockCtrl)
+		mockConfig.EXPECT().SecurityProvider().Return(securityProviderType)
+		mockConfig.EXPECT().SecurityProvider().Return(securityProviderType)
+		mockConfig.EXPECT().SecurityAlgorithm().Return("SHA2")
+		mockConfig.EXPECT().SecurityLevel().Return(256)
+		mockConfig.EXPECT().KeyStorePath().Return("")
+		mockConfig.EXPECT().Ephemeral().Return(true)
+
+		//Get cryptosuite using config
+		c, err := GetSuiteByConfig(mockConfig)
+		if err != nil {
+			t.Fatalf("Not supposed to get error, but got: %v", err)
+		}
+
+		verifySuiteType(t, c, "*sw.impl")
+		mockCtrl.Finish()
+	}
+
 }
 
 func TestCryptoSuiteByConfigPKCS11(t *testing.T) {
 
-	mockCtrl := gomock.NewController(t)
-	defer mockCtrl.Finish()
-
-	//Prepare Config
-	providerLib, softHSMPin, softHSMTokenLabel := pkcs11.FindPKCS11Lib()
-
-	mockConfig := mockcore.NewMockCryptoSuiteConfig(mockCtrl)
-	mockConfig.EXPECT().SecurityProvider().Return("PKCS11")
-	mockConfig.EXPECT().SecurityProvider().Return("PKCS11")
-	mockConfig.EXPECT().SecurityAlgorithm().Return("SHA2")
-	mockConfig.EXPECT().SecurityLevel().Return(256)
-	mockConfig.EXPECT().KeyStorePath().Return("")
-	mockConfig.EXPECT().Ephemeral().Return(true)
-	mockConfig.EXPECT().SecurityProviderLibPath().Return(providerLib)
-	mockConfig.EXPECT().SecurityProviderLabel().Return(softHSMTokenLabel)
-	mockConfig.EXPECT().SecurityProviderPin().Return(softHSMPin)
-	mockConfig.EXPECT().SoftVerify().Return(true)
-
-	//Get cryptosuite using config
-	c, err := GetSuiteByConfig(mockConfig)
-	if err != nil {
-		t.Fatalf("Not supposed to get error, but got: %v", err)
+	// test multiple string cases
+	securityProviderTypes := []string{
+		// upper case
+		"PKCS11",
+		// lower case
+		"pkcs11",
+		// mixed case
+		"Pkcs11",
 	}
 
-	verifySuiteType(t, c, "*pkcs11.impl")
+	for _, securityProviderType := range securityProviderTypes {
+
+		mockCtrl := gomock.NewController(t)
+
+		//Prepare Config
+		providerLib, softHSMPin, softHSMTokenLabel := pkcs11.FindPKCS11Lib()
+
+		mockConfig := mockcore.NewMockCryptoSuiteConfig(mockCtrl)
+		mockConfig.EXPECT().SecurityProvider().Return(securityProviderType)
+		mockConfig.EXPECT().SecurityProvider().Return(securityProviderType)
+		mockConfig.EXPECT().SecurityAlgorithm().Return("SHA2")
+		mockConfig.EXPECT().SecurityLevel().Return(256)
+		mockConfig.EXPECT().KeyStorePath().Return("")
+		mockConfig.EXPECT().Ephemeral().Return(true)
+		mockConfig.EXPECT().SecurityProviderLibPath().Return(providerLib)
+		mockConfig.EXPECT().SecurityProviderLabel().Return(softHSMTokenLabel)
+		mockConfig.EXPECT().SecurityProviderPin().Return(softHSMPin)
+		mockConfig.EXPECT().SoftVerify().Return(true)
+
+		//Get cryptosuite using config
+		c, err := GetSuiteByConfig(mockConfig)
+		if err != nil {
+			t.Fatalf("Not supposed to get error, but got: %v", err)
+		}
+
+		verifySuiteType(t, c, "*pkcs11.impl")
+		mockCtrl.Finish()
+	}
+
 }
 
 func verifySuiteType(t *testing.T, c core.CryptoSuite, expectedType string) {
