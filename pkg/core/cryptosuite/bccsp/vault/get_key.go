16529673ef82389011ae700bdb0f9e3566a2f806
Emil Nikolov <emil.e.nikolov@gmail.com>
2018-05-19 16:10:16 +0200

    [FAB-9831] WIP Add a Vault CryptoSuite implementation
    Change-Id: I80d184a3f31ba546c11d7e5c0f5f1e3b193c24b2
    Signed-off-by: Emil Nikolov <emil.e.nikolov@gmail.com>

---
+++ pkg/core/cryptosuite/bccsp/vault/get_key.go
@@ -0,0 +1,41 @@
+/*
+Copyright Hyperledger and its contributors.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package vault
+
+import (
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/core"
+	"github.com/hyperledger/fabric-sdk-go/pkg/core/cryptosuite/bccsp/vault/internal"
+	"github.com/pkg/errors"
+)
+
+// GetKey returns the key this CSP associates to
+// the Subject Key Identifier ski.
+func (csp *CryptoSuite) GetKey(ski []byte) (core.Key, error) {
+	keyID, err := csp.loadKeyID(ski)
+
+	if err != nil {
+		return nil, err
+	}
+
+	sw, err := csp.getKey(keyID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return sw.ParseKey()
+}
+
+func (csp *CryptoSuite) getKey(keyID string) (*internal.SecretWrapper, error) {
+	secret, err := csp.client.Logical().Read("fabric/transit/keys/" + keyID)
+
+	if err != nil {
+		return nil, errors.Errorf("failed to find key with id `%s`", keyID)
+	}
+
+	return internal.NewSecretWrapper(secret), nil
+}
