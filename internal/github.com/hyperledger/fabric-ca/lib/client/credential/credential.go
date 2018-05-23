6d215a685db26315d0720c67169dacd0c225b165
Sudesh Shetty <sudesh.shetty@securekey.com>
2018-05-23 15:20:26 -0400

    [FAB-10279] pinning script updates for fabric-ca
    - no logic changes in SDK
    
    
    Change-Id: I5f24cc0f30674f1e4ba2067be988cc2adf21e054
    Signed-off-by: Sudesh Shetty <sudesh.shetty@securekey.com>

--- internal/github.com/hyperledger/fabric-ca/lib/client/credential/credential.go
+++ internal/github.com/hyperledger/fabric-ca/lib/client/credential/credential.go
@@ -3,6 +3,10 @@ Copyright IBM Corp. All Rights Reserved.
 
 SPDX-License-Identifier: Apache-2.0
 */
+/*
+Notice: This file has been modified for Hyperledger Fabric SDK Go usage.
+Please review third_party pinning scripts and patches for more details.
+*/
 
 package credential
 
@@ -30,9 +34,9 @@ type Credential interface {
 	Store() error
 	// Loads the credential value from disk and sets the value of this credential
 	Load() error
-	// CreateToken returns authorization token for the specified request with
+	// CreateOAuthToken returns oauth autentication token for that request with
 	// specified body
-	CreateToken(req *http.Request, reqBody []byte) (string, error)
+	CreateOAuthToken(req *http.Request, reqBody []byte) (string, error)
 	// Submits revoke request to the Fabric CA server to revoke this credential
 	RevokeSelf() (*api.RevocationResponse, error)
 }
