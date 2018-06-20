2c2fd980d71edc4d1716c1393232738d4005b06d
Bob Stasyszyn <Bob.Stasyszyn@securekey.com>
2018-06-20 17:05:33 -0400

    [FAB-10667] Resolve additional endorsers for CC-to-CC
    A client may not know the complete invocation chain when invoking a chaincode
    (i.e. the target chaincode may invoke other chaincodes) and therefore the
    transaction may fail due to insufficient endorsements. Additional logic was
    added to the Channel Client to automatically detect additional invoked
    chaincodes by reading the RWSets in the proposal response. If additional
    namespaces are found then the Selection Service recalculates the required
    set of endorsers and requests are sent to those additional endorsers.
    
    Change-Id: I41ec5e63bdbc216266c3ad959263e32127d7d4d1
    Signed-off-by: Bob Stasyszyn <Bob.Stasyszyn@securekey.com>

--- pkg/client/channel/api.go
+++ pkg/client/channel/api.go
@@ -10,6 +10,7 @@ import (
 	reqContext "context"
 	"time"
 
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel/invoke"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/context"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
@@ -25,6 +26,7 @@ type requestOptions struct {
 	Retry         retry.Opts
 	Timeouts      map[fab.TimeoutType]time.Duration //timeout options for channel client operations
 	ParentContext reqContext.Context                //parent grpc context for channel client operations (query, execute, invokehandler)
+	CCFilter      invoke.CCFilter
 }
 
 // RequestOption func for each Opts argument
@@ -132,6 +134,14 @@ func WithTimeout(timeoutType fab.TimeoutType, timeout time.Duration) RequestOpti
 func WithParentContext(parentContext reqContext.Context) RequestOption {
 	return func(ctx context.Client, o *requestOptions) error {
 		o.ParentContext = parentContext
+		return nil
+	}
+}
+
+//WithChaincodeFilter adds a chaincode filter for figuring out additional endorsers
+func WithChaincodeFilter(ccFilter invoke.CCFilter) RequestOption {
+	return func(ctx context.Client, o *requestOptions) error {
+		o.CCFilter = ccFilter
 		return nil
 	}
 }
