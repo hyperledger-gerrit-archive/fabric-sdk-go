50a103e45fd9682a650705a482b173defac34edd
Troy Ronda <troy@troyronda.com>
2018-06-22 17:13:38 -0400

    [FAB-10839] Run unit tests on changed packages
    This change modifies the unit testing script to
    only run tests on changed packages when race
    condition detection is enabled.
    
    Change-Id: I55109e3ff2ce50708953ed55a61cd731c758f36a
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/scripts/check_lint.sh
+++ test/scripts/check_lint.sh
@@ -9,42 +9,50 @@
 set -e
 LINT_CHANGED_ONLY="${LINT_CHANGED_ONLY:-false}"
 GO_CMD="${GO_CMD:-go}"
-GO_PROJECT="github.com/hyperledger/fabric-sdk-go"
+REPO="github.com/hyperledger/fabric-sdk-go"
 GOMETALINT_CMD=gometalinter
 
-# Find all packages that should be linted.
-declare -a src=(
-"./pkg"
-"./test"
-)
+function findPackages {
+    # Find all packages that should be linted.
+    declare -a src=(
+    "./pkg"
+    "./test"
+    )
 
-PKGS=()
-for i in "${src[@]}"
-do
-   PKG_LIST=`$GO_CMD list $i/...`
-   while read -r line; do
-      PKGS+=("$line")
-   done <<< "$PKG_LIST"
-done
+    PKGS=()
+    for i in "${src[@]}"
+    do
+       PKG_LIST=`$GO_CMD list $i/... 2> /dev/null`
+       while read -r line; do
+          PKGS+=("$line")
+       done <<< "$PKG_LIST"
+    done
+}
 
-# Determine which directories have changes.
-CHANGED=$(git diff --name-only --diff-filter=ACMRTUXB HEAD)
+function findChangedPackages {
+    # Determine which directories have changes.
+    CHANGED=$(git diff --name-only --diff-filter=ACMRTUXB HEAD)
 
-if [[ "$CHANGED" != "" ]]; then
-    CHANGED+=$'\n'
-fi
-
-LAST_COMMITS=($(git log -2 --pretty=format:"%h"))
-CHANGED+=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMRTUXB -r ${LAST_COMMITS[1]} ${LAST_COMMITS[0]})
-
-CHANGED_PKGS=()
-while read -r line; do
-    if [ "$line" != "" ]; then
-        DIR=`dirname $line`
-        CHANGED_PKGS+=("$GO_PROJECT/$DIR")
+    if [[ "$CHANGED" != "" ]]; then
+        CHANGED+=$'\n'
     fi
-done <<< "$CHANGED"
-CHANGED_PKGS=($(printf "%s\n" "${CHANGED_PKGS[@]}" | sort -u | tr '\n' ' '))
+
+    LAST_COMMITS=($(git log -2 --pretty=format:"%h"))
+    CHANGED+=$(git diff-tree --no-commit-id --name-only --diff-filter=ACMRTUXB -r ${LAST_COMMITS[1]} ${LAST_COMMITS[0]})
+
+    CHANGED_PKGS=()
+    while read -r line; do
+        if [ "$line" != "" ]; then
+            DIR=`dirname $line`
+            if [ "$DIR" = "." ]; then
+                CHANGED_PKGS+=("$REPO")
+            else
+                CHANGED_PKGS+=("$REPO/$DIR")
+            fi
+        fi
+    done <<< "$CHANGED"
+    CHANGED_PKGS=($(printf "%s\n" "${CHANGED_PKGS[@]}" | sort -u | tr '\n' ' '))
+}
 
 function filterExcludedPackages {
     FILTERED_PKGS=()
@@ -62,20 +70,34 @@ function filterExcludedPackages {
     PKGS=("${FILTERED_PKGS[@]}")
 }
 
+# packagesToDirs convert packages to directories
+function packagesToDirs {
+    DIRS=()
+    for i in "${PKGS[@]}"
+    do
+        lintdir=${i#$REPO/}
+        DIRS+=($lintdir)
+    done
+}
+
+findPackages
+
 # Reduce Linter checks to changed packages.
 if [ "$LINT_CHANGED_ONLY" = true ]; then
+    findChangedPackages
     filterExcludedPackages
 fi
 
-# Convert packages to directories
-echo "Paths to lint:"
-DIRS=()
-for i in "${PKGS[@]}"
-do
-    lintdir=${i#$GO_PROJECT/}
-    DIRS+=($lintdir)
-    echo "  $lintdir"
-done
+packagesToDirs
+
+if [ ${#DIRS[@]} -eq 0 ]; then
+    echo "Skipping linter since no packages were changed"
+    exit 0
+fi
+
+if [ "$LINT_CHANGED_ONLY" = true ]; then
+    echo "Changed directories to lint: ${DIRS[@]}"
+fi
 
 echo "Running metalinters..."
 $GOMETALINT_CMD --config=./gometalinter.json "${DIRS[@]}"
