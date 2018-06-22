899e1b580035c4e6fb506c39bcbac33d221a0912
Troy Ronda <troy@troyronda.com>
2018-06-22 12:43:17 -0400

    [FAB-10838] CI lints only changed packages
    This change updates the linter script to detect modified
    packages, and only run linting on those packages.
    
    A new Make target lint-all has been added to allow for
    linting all packages.
    
    The race detector is now allowed to run on all platforms.
    
    Change-Id: I4b0ce6f2cc06c7b72ccffd96b8e35a27d3821a7f
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/scripts/check_lint.sh
+++ test/scripts/check_lint.sh
@@ -7,23 +7,69 @@
 # This script runs Go linting and vetting tools
 
 set -e
-
-
-
+LINT_CHANGED_ONLY="${LINT_CHANGED_ONLY:-false}"
+GO_CMD="${GO_CMD:-go}"
+GO_PROJECT="github.com/hyperledger/fabric-sdk-go"
 GOMETALINT_CMD=gometalinter
 
-
-PROJECT_PATH=$GOPATH/src/github.com/hyperledger/fabric-sdk-go
-
-declare -a arr=(
+# Find all packages that should be linted.
+declare -a src=(
 "./pkg"
 "./test"
 )
 
+PKGS=()
+for i in "${src[@]}"
+do
+   PKG_LIST=`$GO_CMD list $i/...`
+   while read -r line; do
+      PKGS+=("$line")
+   done <<< "$PKG_LIST"
+done
+
+# Determine which directories have changes.
+CHANGED=$(git diff --name-only --diff-filter=ACMRTUXB HEAD)
+CHANGED_PKGS=()
+
+echo "CHANGED: $CHANGED"
+while read -r line; do
+    echo "$line"
+    DIR=`dirname $line`
+    CHANGED_PKGS+=("$GO_PROJECT/$DIR")
+done <<< "$CHANGED"
+CHANGED_PKGS=($(printf "%s\n" "${CHANGED_PKGS[@]}" | sort -u | tr '\n' ' '))
+
+function filterExcludedPackages {
+    FILTERED_PKGS=()
+
+    for pkg in "${PKGS[@]}"
+    do
+        for i in "${CHANGED_PKGS[@]}"
+        do
+            if [ "$pkg" = "$i" ]; then
+              FILTERED_PKGS+=("$pkg")
+            fi
+        done
+    done
+
+    PKGS=("${FILTERED_PKGS[@]}")
+}
+
+# Reduce Linter checks to changed packages.
+if [ "$LINT_CHANGED_ONLY" = true ]; then
+    filterExcludedPackages
+fi
+
+# Convert packages to directories
+echo "Paths to lint:"
+DIRS=()
+for i in "${PKGS[@]}"
+do
+    lintdir=${i#$GO_PROJECT/}
+    DIRS+=(lintdir)
+    echo "  lintdir"
+done
 
 echo "Running metalinters..."
-for i in "${arr[@]}"
-do
-   echo "Checking $i"
-   $GOMETALINT_CMD --config=./gometalinter.json $i/...
-done
+$GOMETALINT_CMD --config=./gometalinter.json "${DIRS[@]}"
+echo "Metalinters finished successfully"
