27c5250c6766f1daa810eeb34af7f1c566a8969f
Divyank Katira <Divyank.Katira@securekey.com>
2018-06-26 16:23:40 -0400

    WIP2, please ignore
    Change-Id: I413fb32ba4a7376cd09137e83e7d11468e522f60
    Signed-off-by: Divyank Katira <Divyank.Katira@securekey.com>

--- test/fixtures/testdata/src/github.com/example_cc/example_cc.go
+++ test/fixtures/testdata/src/github.com/example_cc/example_cc.go
@@ -1,5 +1,5 @@
 /*
-Copyright IBM Corp. 2016 All Rights Reserved.
+Copyright SecureKey Technologies 2018 All Rights Reserved.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,9 +17,7 @@ limitations under the License.
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"strconv"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
@@ -31,237 +29,135 @@ type SimpleChaincode struct {
 
 // Init ...
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
-	txID := stub.GetTxID()
-	fmt.Printf("[txID %s] ########### example_cc Init ###########\n", txID)
-	_, args := stub.GetFunctionAndParameters()
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var err error
 
-	if len(args) != 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4")
-	}
-
-	// Initialize the chaincode
-	A = args[0]
-	Aval, err = strconv.Atoi(args[1])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	B = args[2]
-	Bval, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Expecting integer value for asset holding")
-	}
-	fmt.Printf("[txID %s] Aval = %d, Bval = %d\n", txID, Aval, Bval)
-
-	// Write the state to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
+	// Write some test data to the ledger
+	err := stub.PutState("testRead", []byte("test"))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	if transientMap, err := stub.GetTransient(); err == nil {
-		if transientData, ok := transientMap["result"]; ok {
-			fmt.Printf("[txID %s] Transient data in 'init' : %s\n", txID, transientData)
-			return shim.Success(transientData)
-		}
-	}
 	return shim.Success(nil)
 
-}
-
-// Query ...
-func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface) pb.Response {
-	return shim.Error("Unknown supported call")
 }
 
 // Invoke ...
-// Transaction makes payment of X units from A to B
 func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	fmt.Printf("[txID %s] ########### example_cc Invoke ###########\n", stub.GetTxID())
 	function, args := stub.GetFunctionAndParameters()
 
-	if function == "invokecc" {
-		return t.invokeCC(stub, args)
+	if len(args) < 1 {
+		return shim.Error("Incorrect number of arguments. Expecting at least 1")
 	}
 
-	if function != "invoke" {
-		return shim.Error("Unknown function call")
-	}
-
-	if len(args) < 2 {
-		return shim.Error("Incorrect number of arguments. Expecting at least 2")
-	}
-
-	if args[0] == "delete" {
-		// Deletes an entity from its state
-		return t.delete(stub, args)
-	}
-
-	if args[0] == "query" {
-		// queries an entity state
+	if function == "query" {
 		return t.query(stub, args)
 	}
-	if args[0] == "move" {
-		eventID := "testEvent"
-		if len(args) >= 5 {
-			eventID = args[4]
-		}
-		if err := stub.SetEvent(eventID, []byte("Test Payload")); err != nil {
-			return shim.Error("Unable to set CC event: testEvent. Aborting transaction ...")
-		}
-		return t.move(stub, args)
+
+	if function == "queryPrivateState" {
+		return t.queryPrivateState(stub, args)
 	}
-	return shim.Error("Unknown action, check the first argument, must be one of 'delete', 'query', or 'move'")
+
+	if function == "setState" {
+		return t.setState(stub, args)
+	}
+
+	if function == "setStateWithRead" {
+		return t.setStateWithRead(stub, args)
+	}
+
+	if function == "setPrivateState" {
+		return t.setPrivateState(stub, args)
+	}
+
+	if function == "setPrivateStateWithRead" {
+		return t.setPrivateStateWithRead(stub, args)
+	}
+
+	return shim.Error("Unknown action")
 }
 
-func (t *SimpleChaincode) move(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	txID := stub.GetTxID()
-	// must be an invoke
-	var A, B string    // Entities
-	var Aval, Bval int // Asset holdings
-	var X int          // Transaction value
-	var err error
-	if len(args) < 4 {
-		return shim.Error("Incorrect number of arguments. Expecting 4, function followed by 2 names and 1 value")
-	}
-
-	A = args[1]
-	B = args[2]
-
-	// Get the state from the ledger
-	// TODO: will be nice to have a GetAllState call to ledger
-	Avalbytes, err := stub.GetState(A)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Avalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Aval, _ = strconv.Atoi(string(Avalbytes))
-
-	Bvalbytes, err := stub.GetState(B)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if Bvalbytes == nil {
-		return shim.Error("Entity not found")
-	}
-	Bval, _ = strconv.Atoi(string(Bvalbytes))
-
-	// Perform the execution
-	X, err = strconv.Atoi(args[3])
-	if err != nil {
-		return shim.Error("Invalid transaction amount, expecting a integer value")
-	}
-	Aval = Aval - X
-	Bval = Bval + X
-	fmt.Printf("[txID %s] Aval = %d, Bval = %d\n", txID, Aval, Bval)
-
-	// Write the state back to the ledger
-	err = stub.PutState(A, []byte(strconv.Itoa(Aval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(B, []byte(strconv.Itoa(Bval)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	if transientMap, err := stub.GetTransient(); err == nil {
-		if transientData, ok := transientMap["result"]; ok {
-			fmt.Printf("[txID %s] Transient data in 'move' : %s\n", txID, transientData)
-			return shim.Success(transientData)
-		}
-	}
-	return shim.Success(nil)
-}
-
-// Deletes an entity from state
-func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) != 1 {
-		return shim.Error("Incorrect number of arguments. Expecting 1")
-	}
-
-	A := args[1]
-
-	// Delete the key from the state in ledger
-	err := stub.DelState(A)
-	if err != nil {
-		return shim.Error("Failed to delete state")
-	}
-
-	return shim.Success(nil)
-}
-
-// Query callback representing the query of a chaincode
 func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var A string // Entities
-	var err error
-
-	if len(args) != 2 {
-		return shim.Error("Incorrect number of arguments. Expecting name of the person to query")
+	if args[0] == "" {
+		return shim.Error("query key is a required argument")
 	}
-
-	A = args[1]
-
-	// Get the state from the ledger
-	Avalbytes, err := stub.GetState(A)
+	resp, err := stub.GetState(args[0])
 	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to get state for " + A + "\"}"
-		return shim.Error(jsonResp)
+		return shim.Error(err.Error())
 	}
 
-	if Avalbytes == nil {
-		jsonResp := "{\"Error\":\"Nil amount for " + A + "\"}"
-		return shim.Error(jsonResp)
-	}
-
-	jsonResp := "{\"Name\":\"" + A + "\",\"Amount\":\"" + string(Avalbytes) + "\"}"
-	fmt.Printf("[txID %s] Query Response:%s\n", stub.GetTxID(), jsonResp)
-	return shim.Success(Avalbytes)
+	return shim.Success(resp)
 }
 
-type argStruct struct {
-	Args []string `json:"Args"`
+func (t *SimpleChaincode) queryPrivateState(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if args[0] == "" {
+		return shim.Error("query key is a required argument")
+	}
+	resp, err := stub.GetPrivateData("billing", args[0])
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(resp)
 }
 
-func asBytes(args []string) [][]byte {
-	bytes := make([][]byte, len(args))
-	for i, arg := range args {
-		bytes[i] = []byte(arg)
+func (t *SimpleChaincode) setState(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 || args[0] == "" || args[1] == "" {
+		return shim.Error("two args are required to setState, key and value")
 	}
-	return bytes
+	err := stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
 }
 
-// invokeCC invokes another chaincode
-// arg0: ID of chaincode to invoke
-// arg1: Chaincode arguments in the form: {"Args": ["arg0", "arg1",...]}
-func (t *SimpleChaincode) invokeCC(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	if len(args) < 2 {
-		return shim.Error("Incorrect number of arguments. Expecting ID of chaincode to invoke and args")
+func (t *SimpleChaincode) setStateWithRead(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 || args[0] == "" || args[1] == "" {
+		return shim.Error("two args are required to setState, key and value")
 	}
 
-	ccID := args[0]
-	invokeArgsJSON := args[1]
-
-	argStruct := argStruct{}
-	if err := json.Unmarshal([]byte(invokeArgsJSON), &argStruct); err != nil {
-		return shim.Error(fmt.Sprintf("Invalid invoke args: %s", err))
+	_, err := stub.GetState("testRead")
+	if err != nil {
+		return shim.Error(err.Error())
 	}
 
-	if err := stub.PutState(stub.GetTxID()+"_invokedcc", []byte(ccID)); err != nil {
-		return shim.Error(fmt.Sprintf("Error putting state: %s", err))
+	err = stub.PutState(args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error(err.Error())
 	}
 
-	return stub.InvokeChaincode(ccID, asBytes(argStruct.Args), "")
+	return shim.Success(nil)
+}
+
+func (t *SimpleChaincode) setPrivateState(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 || args[0] == "" || args[1] == "" {
+		return shim.Error("two args are required to setState, key and value")
+	}
+
+	err := stub.PutPrivateData("billing", args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
+}
+
+func (t *SimpleChaincode) setPrivateStateWithRead(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) != 2 || args[0] == "" || args[1] == "" {
+		return shim.Error("two args are required to setState, key and value")
+	}
+
+	_, err := stub.GetPrivateData("billing", "testRead")
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	err = stub.PutPrivateData("billing", args[0], []byte(args[1]))
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+
+	return shim.Success(nil)
 }
 
 func main() {
