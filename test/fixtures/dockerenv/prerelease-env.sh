f2572f910da540415fd66945927845a9f75cc3f7
Troy Ronda <troy@troyronda.com>
2018-07-05 13:42:26 -0400

    [FAB-11066] Update stable version to 1.2.0
    Change-Id: I061c453456cec75f5f0d80a06b3e2e8ff7246eab
    Signed-off-by: Troy Ronda <troy@troyronda.com>

--- test/fixtures/dockerenv/prerelease-env.sh
+++ test/fixtures/dockerenv/prerelease-env.sh
@@ -15,14 +15,6 @@ export FABRIC_ORDERER_FIXTURE_TAG="1.2.0-rc1"
 export FABRIC_PEER_FIXTURE_TAG="1.2.0-rc1"
 export FABRIC_BUILDER_FIXTURE_TAG="1.2.0-rc1"
 
-export ARCH=""
-export ARCH_SEP=""
-
-# Using default BASSEOS image (until there is a compatibility issue)
-# export FABRIC_BASEOS_FIXTURE_TAG="0.4.6"
-# export FABRIC_BASEIMAGE_FIXTURE_TAG="0.4.6"
-# export FABRIC_COUCHDB_FIXTURE_TAG="0.4.6"
-
 # override SDK configuration that loads crypto-config
 export FABRIC_SDK_CLIENT_CRYPTOCONFIG_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config"
 export FABRIC_SDK_CLIENT_ORDERERS_TLSCACERTS_PATH='${GOPATH}'"/src/github.com/hyperledger/fabric-sdk-go/test/fixtures/fabric/${FABRIC_CRYPTOCONFIG_VERSION}/crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem"
