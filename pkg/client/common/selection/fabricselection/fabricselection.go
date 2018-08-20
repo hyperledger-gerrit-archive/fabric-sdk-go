57911209c8e2383470028f6a7f863d128357fe45
Sandra Vrtikapa <sandra.vrtikapa@securekey.com>
2018-08-20 13:18:04 -0400

    [FABG-724] Don't Query all the peers for selection
    Change-Id: I17805a695f61e04d923cf75e7249d7e7524c54c7
    Signed-off-by: Sandra Vrtikapa <sandra.vrtikapa@securekey.com>

--- pkg/client/common/selection/fabricselection/fabricselection.go
+++ pkg/client/common/selection/fabricselection/fabricselection.go
@@ -27,6 +27,7 @@ import (
 	fabdiscovery "github.com/hyperledger/fabric-sdk-go/pkg/fab/discovery"
 	"github.com/hyperledger/fabric-sdk-go/pkg/util/concurrent/lazycache"
 	"github.com/hyperledger/fabric-sdk-go/pkg/util/concurrent/lazyref"
+	"github.com/hyperledger/fabric-sdk-go/pkg/util/random"
 	"github.com/pkg/errors"
 	grpcCodes "google.golang.org/grpc/codes"
 )
@@ -226,7 +227,7 @@ func (s *Service) query(req *discclient.Request, chaincodes []*fab.ChaincodeCall
 	responses, err := s.discClient.Send(reqCtx, req, targets...)
 	if err != nil {
 		if len(responses) == 0 {
-			return nil, errors.Wrapf(err, "error calling discover service send")
+			return nil, errors.Wrapf(err, "error calling discover service send for selection")
 		}
 		logger.Warnf("Received %d response(s) and one or more errors from discovery client: %s", len(responses), err)
 	}
@@ -270,17 +271,19 @@ func (s *Service) query(req *discclient.Request, chaincodes []*fab.ChaincodeCall
 }
 
 func (s *Service) getTargets(ctx contextAPI.Client) ([]fab.PeerConfig, error) {
-	// TODO: The number of peers to query should be retrieved from the channel policy.
-	// This will done in a future patch.
+
 	chpeers, ok := ctx.EndpointConfig().ChannelPeers(s.channelID)
 	if !ok {
 		return nil, errors.Errorf("failed to get peer configs for channel [%s]", s.channelID)
 	}
-	targets := make([]fab.PeerConfig, len(chpeers))
-	for i := 0; i < len(targets); i++ {
-		targets[i] = chpeers[i].NetworkPeer.PeerConfig
+
+	chConfig, ok := ctx.EndpointConfig().ChannelConfig(s.channelID)
+	if !ok {
+		return nil, errors.Errorf("failed to get channel endpoint config for channel [%s]", s.channelID)
 	}
-	return targets, nil
+
+	//pick number of peers based on channel policy
+	return random.PickRandomNPeerConfigs(chpeers, chConfig.Policies.QueryChannelConfig.QueryDiscovery), nil
 }
 
 func asChaincodeInterests(chaincodes []*fab.ChaincodeCall) *discovery.ChaincodeInterest {
