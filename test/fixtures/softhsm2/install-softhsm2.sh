40e131ca13b7f3df9d96888521be5f9b2d88512d
Brett Logan <Brett.T.Logan@ibm.com>
2019-05-23 15:00:48 -0400

    [FABG-xxxxx] Remove Fabric baseimage
    Signed-off-by: Brett Logan <Brett.T.Logan@ibm.com>
    Change-Id: Ie6aeb9b936f84d9e553c57566ef254a6c50cebec

--- test/fixtures/softhsm2/install-softhsm2.sh
+++ test/fixtures/softhsm2/install-softhsm2.sh
@@ -12,13 +12,26 @@ if [ $ARCH = "s390x" ]; then
   echo "deb http://ftp.us.debian.org/debian sid main" >> /etc/apt/sources.list
 fi
 
+# ----------------------------------------------------------------
+# Install Golang
+# ----------------------------------------------------------------
 apt-get update
+apt-get install -y -qq wget
+mkdir -p $GOPATH
+ARCH=`uname -m | sed 's|i686|386|' | sed 's|x86_64|amd64|'`
+
+cd /tmp
+wget --quiet --no-check-certificate https://storage.googleapis.com/golang/go${GOVER}.linux-${ARCH}.tar.gz
+tar -xvf go${GOVER}.linux-${ARCH}.tar.gz
+mv go $GOROOT
+chmod 775 $GOROOT
+
 apt-get install -y --no-install-recommends softhsm2 curl git gcc g++ libtool libltdl-dev
 mkdir -p /var/lib/softhsm/tokens/
 softhsm2-util --init-token --slot 0 --label "ForFabric" --so-pin 1234 --pin 98765432
 
-# Temporary fix for Fabric base image
-unset GOCACHE
+## Temporary fix for Fabric base image
+#unset GOCACHE
 
 cd /opt/workspace/pkcs11helper/
 go install pkcs11helper/cmd/pkcs11helper
